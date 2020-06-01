7484d0ed6999a7666cdad9c358948174e736dbf8
Kiran Mudili <kiran.mudili@epsoftinc.com>
2020-06-01 19:04:38 +0530

    [RPA-172] RPA-173,174,175 are also covered in this commit

--- src/app/pages/rpautomation/rpautomation-routing.module.ts
+++ src/app/pages/rpautomation/rpautomation-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RpautomationComponent } from './rpautomation.component';
 import { RpaStudioComponent } from './rpa-studio/rpa-studio.component';
-
+import {EnvironmentsComponent} from './environments/environments.component';
 const routes: Routes = [
     {path:'', component:RpautomationComponent, children:[
       {path:'home', component:RpaStudioComponent},
+      {path:'environments',  component:EnvironmentsComponent},
       {path:'**', redirectTo:'/home', pathMatch: 'full'}
     ]}
   ];
--- src/app/pages/rpautomation/rpautomation.module.ts
+++ src/app/pages/rpautomation/rpautomation.module.ts
@@ -7,24 +7,30 @@ import { DndModule } from 'ngx-drag-drop';
 import {  ContextMenuModule } from 'ngx-contextmenu';
 import { DynamicFormBuilderModule } from './dynamic-form-builder/dynamic-form-builder.module';
 import { RpaStudioTabsComponent } from './rpa-studio-tabs/rpa-studio-tabs.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RpaStudioWorkspaceComponent } from './rpa-studio-workspace/rpa-studio-workspace.component';
 import { RpaStudioActionsComponent } from './rpa-studio-actions/rpa-studio-actions.component';
+import { EnvironmentsComponent } from './environments/environments.component';
+import { DataTablesModule } from 'angular-datatables';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 
 
 @NgModule({
-  declarations: [RpautomationComponent, RpaStudioComponent, RpaStudioTabsComponent, RpaStudioWorkspaceComponent, RpaStudioActionsComponent],
+  declarations: [RpautomationComponent, RpaStudioComponent, RpaStudioTabsComponent, RpaStudioWorkspaceComponent, RpaStudioActionsComponent, EnvironmentsComponent],
   imports: [
     CommonModule,
     RpautomationRoutingModule,
     DndModule,
+    DataTablesModule,
     NgbModule,
     ContextMenuModule.forRoot(),
     DynamicFormBuilderModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     FormsModule
   ]
 })
