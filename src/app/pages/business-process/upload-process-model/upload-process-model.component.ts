7b3fe39474cdf48107fc33650a6bf21a0840fe7b
Swaroop Attaluri <swaroop.attaluri@epsoftinc.com>
2020-06-12 18:54:19 +0530

    upload changes

--- src/app/pages/business-process/upload-process-model/upload-process-model.component.ts
+++ src/app/pages/business-process/upload-process-model/upload-process-model.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { diff } from 'bpmn-js-differ';
+import { NgxSpinnerService } from "ngx-spinner"; 
+
 import BpmnModdle from 'bpmn-moddle';
 import * as BpmnJS from 'bpmn-js/dist/bpmn-modeler.production.min.js';
 import { SplitComponent, SplitAreaDirective } from 'angular-split';
-
+import { BpmnModel } from '../model/bpmn-autosave-model';
 import { SharebpmndiagramService } from '../../services/sharebpmndiagram.service';
 import { RestApiService } from '../../services/rest-api.service';
 import { DataTransferService } from '../../services/data-transfer.service';
@@ -14,7 +16,7 @@ import { DataTransferService } from '../../services/data-transfer.service';
   templateUrl: './upload-process-model.component.html',
   styleUrls: ['./upload-process-model.component.css']
 })
-export class UploadProcessModelComponent implements OnInit {
+export class UploadProcessModelComponent implements OnInit,AfterViewInit {
    hideUploadContainer:boolean=false;
    hideCreateContainer:boolean=false;
    hideOptionsContainer:boolean=true;
@@ -35,13 +37,24 @@ export class UploadProcessModelComponent implements OnInit {
   split: SplitComponent;
   area1: SplitAreaDirective;
   area2: SplitAreaDirective;
+  last_updated_time = new Date().getTime();
+  bpmnModel:BpmnModel = new BpmnModel();
+  uploaded_xml:any;
+  counter:number = 0;
+  autosaveObj:any
 
-   constructor(private rest:RestApiService, private bpmnservice:SharebpmndiagramService,private router:Router, 
+
+
+
+   constructor(private rest:RestApiService, private bpmnservice:SharebpmndiagramService,private router:Router, private spinner:NgxSpinnerService,
       private dt:DataTransferService) { }
  
    ngOnInit() {
     this.dt.changeParentModule({"route":"/pages/businessProcess/home", "title":"Business Process Studio"});
     this.dt.changeChildModule({"route":"/pages/businessProcess/uploadProcessModel", "title":"Studio"});
+    
+   }
+   ngAfterViewInit(){
     this.rest.getBPMNFileContent("assets/resources/"+this.bpmnservice.getBpmnData()).subscribe(res => {
       let _self = this;
       this.bpmnModeler = new BpmnJS({
@@ -56,10 +69,116 @@ export class UploadProcessModelComponent implements OnInit {
         }
       })
       this.bpmnModeler.on('element.changed', function(){
-        alert();        
+        let now = new Date().getTime();
+        if(now - _self.last_updated_time > 10*1000){
+          _self.autoSaveDiagram();
+          _self.last_updated_time = now;
+        }
       })
+      
     });
+
+     
    }
+   autoSaveDiagram(){
+    let _self = this;
+    this.bpmnModeler.saveXML({ format: true }, function(err, xml) {
+      _self.uploaded_xml=xml
+           _self.bpmnModel.bpmnXmlNotation = btoa(unescape(encodeURIComponent(xml)))
+    })
+    
+    this.bpmnModel.bpmnModelId=1;
+    this.bpmnModel.bpmnModelModifiedBy="gopi";
+    this.bpmnModel. bpmnModelModifiedTime= new Date();
+    this.bpmnModel.bpmnModelTempStatus="INPROGRESS";
+    this.bpmnModel.bpmnModelTempVersion="v1.0";
+    _self.bpmnModel.bpmnProcessMeta =btoa(unescape(encodeURIComponent(this.uploaded_xml)))
+    
+    this.rest.autoSaveBPMNFileContent(this.bpmnModel).subscribe(
+
+      data=>{;
+        this.autosaveObj=data
+    this.bpmnModel.bpmnModelTempId=this.autosaveObj.bpmnModelTempId
+        
+        this.counter++;
+        this.spinner.hide()
+      },
+      err => {
+          this.spinner.hide();
+        }
+    )
+    
+    
+
+  }
+   saveprocess(){
+    let _self = this;
+    this.bpmnModeler.saveXML({ format: true }, function(err, xml) {
+      _self.uploaded_xml=xml
+           _self.bpmnModel.bpmnXmlNotation = btoa(unescape(encodeURIComponent(xml)))
+    })
+    
+    this.bpmnModel.approverName="vaidehi";
+    
+    this.bpmnModel.bpmnModelId=1,
+  this.bpmnModel.bpmnProcessName="aaaaaa";
+  // this.bpmnModel.bpmnXmlNotation=btoa(_self.uploaded_xml),
+    
+    this.bpmnModel.userName="gopi";
+    this.rest.saveBPMNprocessinfofromtemp(this.bpmnModel).subscribe(
+
+      data=>{
+        
+        alert("saved successfully")
+        
+        this.spinner.hide()
+      },
+      err => {
+        alert("failed")
+          this.spinner.hide();
+        }
+    )
+    
+   }
+   submitDiagramForApproval(){
+    let _self = this;
+    this.bpmnModel.approverName="vaidehi";
+    this.bpmnModel.bpmnJsonNotation= btoa(unescape(encodeURIComponent(this.uploaded_xml)));
+    this.bpmnModel.bpmnModelId=1;
+    this.bpmnModel.bpmnNotationAutomationTask=btoa(unescape(encodeURIComponent(this.uploaded_xml)));
+    this.bpmnModel.bpmnNotationHumanTask=btoa(unescape(encodeURIComponent(this.uploaded_xml)));
+    this.bpmnModel.bpmnProcessApproved=0;
+    this.bpmnModel.bpmnProcessName="pizza diagram";
+    this.bpmnModel.bpmnProcessStatus="open";
+    this.bpmnModel.bpmnModelTempId=this.autosaveObj.bpmnModelTempId;
+    this.bpmnModel.bpmnXmlNotation=btoa(unescape(encodeURIComponent(this.uploaded_xml)));
+    this.bpmnModel.category= "string";
+    this.bpmnModel.emailTo= "swaroop.attaluri@epsoftinc.com";
+    this.bpmnModel.id= 5;
+    this.bpmnModel.processIntelligenceId= 5;
+    this.bpmnModel.bpmnJsonNotationreviewComments ="asdf";
+    this.bpmnModel.tenantId=7;
+    this.bpmnModel.userName="gopi";
+    this.spinner.show()
+    this.rest.submitBPMNforApproval(this.bpmnModel).subscribe(
+
+      data=>{
+        
+        alert("saved successfully")
+        
+        this.spinner.hide()
+      },
+      err => {
+        alert("failed")
+          this.spinner.hide();
+        }
+    )
+    
+   
+
+    
+  
+  }
 
    uploadConfBpmn(confBpmnData){
     this.confBpmnModeler = new BpmnJS({
