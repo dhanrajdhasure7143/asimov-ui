53342692cbfa038ed8105ee4c86a62871380f95c
Venkata Siva Gopi Simhadri <venkata.simhadri@epsoftinc.global>
2020-07-30 20:03:19 +0530

    [AIOTAL-319] , added changes for role based ui access for 2.0 product

--- src/app/pages/business-process/home/home.component.ts
+++ src/app/pages/business-process/home/home.component.ts
@@ -28,11 +28,26 @@ export class BpsHomeComponent implements OnInit {
   xpandStatus=false;
   autosavedDiagramList = [];
   autosavedDiagramVersion = [];
+  public userRole:any = [];
+  public isButtonVisible = false;
+
 
   constructor(private router:Router, private bpmnservice:SharebpmndiagramService, private dt:DataTransferService,
      private rest:RestApiService, private hints:BpsHints ) { }
 
   ngOnInit(){
+    this.userRole = localStorage.getItem("userRole")
+    
+    if(this.userRole.includes('SuperAdmin')){
+      this.isButtonVisible = true;
+    }else if(this.userRole.includes('Admin')){
+      this.isButtonVisible = true;
+    }else if(this.userRole.includes('Process Architect')){
+      this.isButtonVisible = true;
+    }else{
+      this.isButtonVisible = false;
+    }
+
     this.isLoading = true;
     this.dt.changeParentModule({"route":"/pages/businessProcess/home", "title":"Business Process Studio"});
     this.dt.changeChildModule({"route":"/pages/businessProcess/home","title":"BPMN Upload"});
--- src/app/pages/rpautomation/rpa-studio-actions/rpa-studio-actions.component.ts
+++ src/app/pages/rpautomation/rpa-studio-actions/rpa-studio-actions.component.ts
@@ -155,6 +155,8 @@ export class RpaStudioActionsComponent implements OnInit {
     
   }
 
+  //loadpredefinedbot(){}
+  versionChange(ver){}
 
 
   reset()
