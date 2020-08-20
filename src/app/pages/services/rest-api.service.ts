547859a1f685d4577dcb97d60a77851e13f1f04c
Charitha Peddu <charitha.peddu@epsoftinc.global>
2020-08-20 20:54:15 +0530

    Changes made in Schedulers

--- src/app/pages/services/rest-api.service.ts
+++ src/app/pages/services/rest-api.service.ts
@@ -34,8 +34,11 @@ export class RestApiService{
   public fileName = new BehaviorSubject<any>('file');
   constructor(private http:HttpClient) { }
   getAccessToken(){
+ 
     let data = {"userId":"jitendra.kothamasu@epsoftinc.com",
                 "password":"Welcome@123"};
+
+  
     return this.http.post('/api/login/beta/accessToken',data);
   }
   bpmnlist(){
