002f0b3abea806d321a3766750969ef19628e2bf
Nagaraju Joneboina <nagaraju.joneboina@epsoftinc.global>
2020-10-09 15:51:56 +0530

    bubble chart updated

--- src/app/pages/process-intelligence/processinsights/processinsights.component.ts
+++ src/app/pages/process-intelligence/processinsights/processinsights.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
  import * as Highcharts from 'highcharts';
 import { RestApiService } from '../../services/rest-api.service';
-
+import HC_more from 'highcharts/highcharts-more' //module
+HC_more(Highcharts) 
  enum VariantList {
   'Most Common',
   'Least Common',
@@ -55,18 +56,23 @@ export class ProcessinsightsComponent implements OnInit {
           }
       },
       title: {
-          text: 'Wind speed during two days',
+          text: 'Human Cost Vs Bot Cost',
           align: 'center'
       },
       xAxis: {
-          type: 'datetime',
+         // type: 'number',
           labels: {
               overflow: 'justify'
-          }
+          },
+          categories: [
+           10,50,100,150,200,250,300,350,400,450,500]
       },
       yAxis: {
           title: {
-              text: 'Wind speed'
+              text: 'Price',
+              labels: {
+                overflow: 'justify'
+            }
           },
           minorGridLineWidth: 0,
           gridLineWidth: 1,
@@ -75,7 +81,8 @@ export class ProcessinsightsComponent implements OnInit {
           // plotBands: []
       },
       tooltip: {
-          valueSuffix: 'm/s'
+          //valueSuffix: '$',
+          valuePrefix: '$'
       },
       plotOptions: {
           spline: {
@@ -86,19 +93,19 @@ export class ProcessinsightsComponent implements OnInit {
                   }
               },
               marker: {
-                  enabled: false
+                  enabled: true
               },
               // pointInterval: 3600000, // one hour
               // pointStart: Date.UTC(2018, 1, 13, 0, 0, 0)
           }
       },
       series: [{
-          name: 'Hestavollane',
-          data: [0, 3, 9, 4, 5, 8, 4.0, 10,6.9]
+          name: 'Human Cost',
+          data: [20, 50, 100, 250, 280, 320, 370, 430,500]
   
       }, {
-          name: 'Vik',
-          data: [0, 3.5, 1, 5, 8, 10, 11,9,11]
+          name: 'Bot Cost',
+          data: [10, 70, 180, 250, 290, 300, 390, 460,500]
       }],
       navigation: {
           menuItemStyle: {
@@ -173,76 +180,153 @@ export class ProcessinsightsComponent implements OnInit {
   addchart2()
   {
     this.chart2={
-      chart: {
-          type: 'scatter',
-          zoomType: 'xy'
-      },
-      title: {
-          text: 'Height Versus Weight of 507 Individuals by Gender'
-      },
-      subtitle: {
-          text: 'Source: Heinz  2003'
-      },
-      xAxis: {
-          title: {
-              enabled: true,
-              text: 'Height (cm)'
-          },
-          startOnTick: true,
-          endOnTick: true,
-          showLastLabel: true
-      },
-      yAxis: {
-          title: {
-              text: 'Weight (kg)'
-          }
-      },
-      legend: {
-          layout: 'vertical',
-          align: 'left',
-          verticalAlign: 'top',
-          x: 100,
-          y: 70,
-          floating: true,
-          backgroundColor: Highcharts.defaultOptions.chart.backgroundColor,
-          borderWidth: 1
-      },
-      plotOptions: {
-          scatter: {
-              marker: {
-                  radius: 5,
-                  states: {
-                      hover: {
-                          enabled: true,
-                          lineColor: 'rgb(100,100,100)'
-                      }
-                  }
-              },
-              states: {
-                  hover: {
-                      marker: {
-                          enabled: false
-                      }
-                  }
-              },
-              tooltip: {
-                  headerFormat: '<b>{series.name}</b><br>',
-                  pointFormat: '{point.x} cm, {point.y} kg'
-              }
-          }
-      },
-      series: [{
-          name: 'Female',
-          color: 'rgba(223, 83, 83, .5)',
-          data: [[161.2, 51.6], [167.5, 59.0], [159.5, 49.2], [157.0, 63.0], ]
-  
-      }, {
-          name: 'Male',
-          color: 'rgba(119, 152, 191, .5)',
-          data: [[174.0, 65.6]]
-      }]
+        chart: {
+            type: 'bubble',
+            plotBorderWidth: 1,
+            zoomType: 'xy'
+        },
+    
+        legend: {
+            enabled: false
+        },
+    
+        title: {
+            text: 'Sugar and fat intake per country'
+        },
+    
+        subtitle: {
+           // text: 'Source: <a href="http://www.euromonitor.com/">Euromonitor</a> and <a href="https://data.oecd.org/">OECD</a>'
+        },
+    
+        accessibility: {
+            point: {
+              //  valueDescriptionFormat: '{index}. {point.name}, fat: {point.x}g, sugar: {point.y}g, obesity: {point.z}%.'
+            }
+        },
+    
+        xAxis: {
+            gridLineWidth: 1,
+            title: {
+                text: 'Daily fat intake'
+            },
+            labels: {
+                format: '{value} gr'
+            },
+            plotLines: [{
+                color: 'black',
+                dashStyle: 'dot',
+                width: 2,
+                value: 65,
+                label: {
+                    rotation: 0,
+                    y: 15,
+                    style: {
+                        fontStyle: 'italic'
+                    },
+                    text: 'Safe fat intake 65g/day'
+                },
+                zIndex: 3
+            }],
+            accessibility: {
+                rangeDescription: 'Range: 60 to 100 grams.'
+            }
+        },
+    
+        yAxis: {
+            startOnTick: false,
+            endOnTick: false,
+            title: {
+                text: 'Daily sugar intake'
+            },
+            labels: {
+                format: '{value} gr'
+            },
+            maxPadding: 0.2,
+            plotLines: [{
+                color: 'black',
+                dashStyle: 'dot',
+                width: 2,
+                value: 50,
+                label: {
+                    align: 'right',
+                    style: {
+                        fontStyle: 'italic'
+                    },
+                    text: 'Safe sugar intake 50g/day',
+                    x: -10
+                },
+                zIndex: 3
+            }],
+            accessibility: {
+                rangeDescription: 'Range: 0 to 160 grams.'
+            }
+        },
+    
+        tooltip: {
+            useHTML: true,
+            headerFormat: '<table>',
+            pointFormat: '<tr><th colspan="2"><h3>{point.country}</h3></th></tr>' +
+                '<tr><th>Fat intake:</th><td>{point.x}g</td></tr>' +
+                '<tr><th>Sugar intake:</th><td>{point.y}g</td></tr>' +
+                '<tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>',
+            footerFormat: '</table>',
+            followPointer: true
+        },
+    
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+    
+        series: [{
+            data: [
+                { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
+                { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
+                { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
+                { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
+                { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
+                { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
+                { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
+                { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
+                { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
+                { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
+                { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
+                { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
+                { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
+                { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
+                { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
+            ]
+        },
+        {
+            data: [
+                { x: 96, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
+                { x: 89.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
+                { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
+                { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
+                { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
+                { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
+                { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
+                { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
+                { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
+                { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
+                { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
+                { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
+                { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
+                { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
+                { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
+            ]
+        }
+    ]
+    
   }
+              
+
   Highcharts.chart('scatter',this.chart2); 
+  
   
 }
 
