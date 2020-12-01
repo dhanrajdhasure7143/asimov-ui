ee83dd6c86ff7352fe592bc7d54428b7b54885d7
MeKiranMudili <69102316+MeKiranMudili@users.noreply.github.com>
2020-12-01 17:03:37 +0530

    [RPA-621] SO Enhancements are done

--- src/app/pages/service-orchestration/service-orchestration.component.ts
+++ src/app/pages/service-orchestration/service-orchestration.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-service-orchestration',
   template: `<div class="module-heading">
-              <div class="container">
+              <div class="container-fluid">
                 <img class="module-heading-image" src='..\\assets\\busineeprocessstudionewicon.svg'>
                 <span class="module-heading-title">Service Orchestration</span>
               </div>
