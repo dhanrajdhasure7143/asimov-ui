29c54e87c1612082eb1a2fa221c810a7bb802d83
Nagaraju Joneboina <nagaraju.joneboina@epsoftinc.global>
2021-02-01 17:27:58 +0530

    camunda redirection url updated in bps home page

--- src/app/pages/business-process/home/home.component.ts
+++ src/app/pages/business-process/home/home.component.ts
@@ -252,7 +252,7 @@ export class BpsHomeComponent implements OnInit {
     let userId = localStorage.getItem("ProfileuserId");
     let splitTenant:any;
     if(selecetedTenant){
-       splitTenant = selecetedTenant.split('-')[0];
+       splitTenant = selecetedTenant.split('-')[0]; 
     }
     window.location.href = "http://172.16.0.73:8080/camunda/app/welcome/"+splitTenant+"/#!/login?accessToken=" + token + "&userID="+userId+"&tenentID="+selecetedTenant;
    //var token=localStorage.getItem('accessToken');
