d215251fcd484936e0209eecf23002a8c4004ece
Nagaraju Joneboina <nagaraju.joneboina@epsoftinc.com>
2021-02-08 13:08:27 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -34,7 +34,7 @@ export const environment = {
 
 
   //BPMN Platform URL
-  bpmnplatformUrl : "http://172.16.0.73:8080"
+  bpmnplatformUrl : "https://eiapcamundademo.epsoftinc.com:86"
 
  //  * QA ENV URl's
  
