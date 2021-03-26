9690a8d4d39286bae226d545b6619f9bb796ef84
Ranjith Sigiri <ranjith.sigiri@epsoftinc.global>
2021-03-26 13:14:32 +0530

    redirection url ,white space and currency pipe

--- src/app/rediraction/redirection.component.ts
+++ src/app/rediraction/redirection.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AuthenticationService } from '../services';
-
+import { APP_CONFIG } from 'src/app/app.config';
 @Component({
     selector: 'app-redirection',
     templateUrl: './redirection.component.html'
 })
 export class RedirectionComponent{
-  constructor(private authService:AuthenticationService) { 
+  constructor(private authService:AuthenticationService, @Inject(APP_CONFIG) private config) { 
     this.authService.logout();
       localStorage.clear();
-      window.location.href='http://eiapclouddev.epsoftinc.in/';
+      window.location.href=this.config.camunda_redirectionLogout;
   }
   ngOnInit() {}
 }
