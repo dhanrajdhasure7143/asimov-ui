5aca33d93d57113c795d9d51c4b09e738ac8078c
Deepak <deepak.pilla@epsoftinc.com>
2021-03-29 18:01:16 +0530

    [merging to qa]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -58,7 +58,7 @@ export const environment = {
   // Aiotal redirection urls 
     myaccount_url : "http://eiapcloudqa.epsoftinc.in/#/activation",
     logout_url: 'http://eiapcloudqa.epsoftinc.in/#/activation',
-    redirection_logout_url: 'http://eiapclouddev.epsoftinc.in/#/user',
+    redirection_logout_url: 'http://eiapcloudqa.epsoftinc.in/#/user',
 
     // PI Connectors
     connector_topic: 'nodeq',
