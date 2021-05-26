ad946246b23d5ee41346cd8ba7fa18874d326b8f
Deepak Pilla <deepak.pilla@epsoftinc.com>
2021-05-26 17:43:47 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -60,6 +60,7 @@ export const environment = {
     myaccount_url : "http://eiapcloudqa.epsoftinc.in/#/activation",
     logout_url: 'http://eiapcloudqa.epsoftinc.in/#/activation',
     redirectout_url: 'http://eiapcloudqa.epsoftinc.in/#/signout',
+    platform_home_url: 'http://eiapcloudqa.epsoftinc.in/#/',
 
     // PI Connectors
     connector_topic: 'nodeq',
