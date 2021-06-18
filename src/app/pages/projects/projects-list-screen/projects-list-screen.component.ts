79e43df16d2598cbd9ade4b934b1809829bfd5b6
Venkata Siva Gopi Simhadri <venkata.simhadri@epsoftinc.global>
2021-06-18 16:26:05 +0530

    [PLATFORM-843] , added changes for List of projects screen

--- src/app/pages/projects/projects-list-screen/projects-list-screen.component.ts
+++ src/app/pages/projects/projects-list-screen/projects-list-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatDialog, MatTabGroup} from '@angular/material';
 
 @Component({
   selector: 'app-projects-list-screen',
@@ -7,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsListScreenComponent implements OnInit {
 
-  constructor() { }
 
   ngOnInit() {
   }
