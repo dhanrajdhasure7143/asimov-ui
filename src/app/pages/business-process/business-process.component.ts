3aaa2fc8302e17445f9bb5c3db1354f4b5f3c3e2
siva.medipudi <siva.medipudi@epsoftinc.com>
2021-07-02 23:01:19 +0530

    -BPS-Screen-Heade-Changes

--- src/app/pages/business-process/business-process.component.ts
+++ src/app/pages/business-process/business-process.component.ts
@@ -4,11 +4,31 @@ import { Router, ActivatedRoute } from '@angular/router';
   selector: 'app-bussiness-process',
   template: `<div class="main-content">
               <div class="row content-area">
-                <div class="module-heading">
-                  <i class="fa fa-arrow-left" aria-hidden="true" (click)="route()"></i>&nbsp;&nbsp;&nbsp;
-                  <img class="module-heading-image" src='..\\assets\\busineeprocessstudionewicon.svg'>
-                  <span class="module-heading-title">{{isShowConformance ? 'Process Intelligence' : 'Business Process Studio' }}</span>
-                </div>
+                <div class=" row module-heading title">
+                    <div class="col-md-6 new-bps-header-title">
+                        <span class="module-heading-title">Business Process Studio</span>
+                            <div class="value">
+                                <span class="level3"><strong>Level 3</strong> - Value Chain Acneage</span>
+                                <span class="level3">Last Modified By: Peter James</span>
+                                <span>Auto Save: April-09-2021</span>
+                            </div> 
+                    </div>
+                    <div class="col-md-6 new-bps-icons">
+                      <div class="row">
+                        <button><img src="../assets/images/BPMS/Edit.svg"></button>
+                        <button><img src="../assets/images/BPMS/Approve.svg"></button>
+                        <button><img src="../assets/images/BPMS/download.svg"></button>
+                        <button><img src="../assets/images/BPMS/share.svg"></button>
+                        <button><img src="../assets/images/BPMS/Forward.svg"></button>
+                        <button><img src="../assets/images/BPMS/copy.svg"></button>
+                        <button><img src="../assets/images/BPMS/Favorite.svg"></button>
+                        <button class="bps-save-icon"><img src="../assets/images/BPMS/save.svg"></button>
+                        <button><img src="../assets/images/BPMS/refresh.svg"></button>
+                        <button><img src="../assets/images/BPMS/Delete.svg"></button>
+                        <button class="upload-btn bps-upload"><img src="../assets/images/BPMS/upload.svg">Upload</button>
+                      </div>  
+                    </div>
+                </div>    
                 <router-outlet></router-outlet>
               </div>
             </div>`,
@@ -32,3 +52,6 @@ export class BusinessProcessComponent implements AfterViewChecked {
 
 
 }
+// <i class="fa fa-arrow-left" aria-hidden="true" (click)="route()"></i>&nbsp;&nbsp;&nbsp;
+//                   <span class="module-heading-title">Business Process Studio</span>
+//                   
