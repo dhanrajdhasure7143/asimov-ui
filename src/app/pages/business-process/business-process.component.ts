50d32a5511260d292e1c3e9859605157b827272e
siva.medipudi <siva.medipudi@epsoftinc.com>
2021-07-09 19:31:24 +0530

    -BPS-Screen-Changes

--- src/app/pages/business-process/business-process.component.ts
+++ src/app/pages/business-process/business-process.component.ts
@@ -2,41 +2,7 @@ import { Component, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-bussiness-process',
-  template: `<div class="main-content">
-              <div class="row content-area">
-                <div class=" row module-heading title">
-                    <div class="col-md-6 new-bps-header-title">
-                        <span class="module-heading-title">Business Process Studio</span>
-                            <div class="value" *ngIf="isHeaderShow == 'true'">
-                                <span class="level3"><strong>Level 3</strong> - Value Chain Acneage</span>
-                                <span class="level3">Last Modified By: Peter James</span>
-                                <span>Auto Save: April-09-2021</span>
-                            </div> 
-                    </div>
-                    <div class="col-md-6 new-bps-icons" *ngIf="isHeaderShow == 'true'">
-                      <div class="row bps-float">
-                        <button><img src="../assets/images/BPMS/Edit-hover.svg"></button>
-                        <select class="dowload-images">
-                          <option value="svg">SVG</option>
-                          <option value="png">PNG</option>
-                          <option value="jpg">JPG</option>
-                          <option value="bpmn">BPMN</option>
-                        </select>
-                        <button (click)="zoomIn()" class="btn btn1 zoom btn-primary" id="zoom_in"><i class="fas fa-search-plus"></i></button>
-                        <button (click)="zoomOut()" class="btn btn1 zoom-out btn-primary" id="zoom_out"><i class="fas fa-search-minus"></i></button>
-                        <button><img src="../assets/images/BPMS/share.svg"></button>
-                        <button><img src="../assets/images/BPMS/Forward.svg"></button>
-                        <button><img src="../assets/images/BPMS/copy.svg"></button>
-                        <button class="bps-save-icon"><img src="../assets/images/BPMS/save.svg"></button>
-                        <button><img src="../assets/images/BPMS/refresh.svg"></button>
-                        <button><img src="../assets/images/BPMS/Delete.svg"></button>
-                        <button class="upload-btn bps-upload"><img src="../assets/images/BPMS/upload.svg">Upload</button>
-                      </div>  
-                    </div>
-                </div>    
-                <router-outlet></router-outlet>
-              </div>
-            </div>`,
+  templateUrl: './business-process.component.html' ,
   styleUrls: ['./business-process.component.css']
 })
 export class BusinessProcessComponent implements AfterViewChecked {
