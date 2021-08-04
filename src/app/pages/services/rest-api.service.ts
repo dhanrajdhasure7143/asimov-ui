2d9173b0355deb87e12e835510fe024e0049355a
Salma Shaik <salma.shaik@epsoftinc.global>
2021-08-04 18:15:28 +0530

    [PLATFORM-936] Added latest documents list dynamically in project details page

--- src/app/pages/services/rest-api.service.ts
+++ src/app/pages/services/rest-api.service.ts
@@ -925,6 +925,9 @@ save_blueprism_config(data)
   getTaskAttachments(projectid,taskid){
     return this.http.get("/platform-service/document/tasksAttachments/"+projectid+"/"+taskid+"")
   }
+  getLatestfiveAttachments(projectid,timezone){
+    return this.http.get("/platform-service/document/uploadedFilespaged/"+projectid,timezone)
+  }
   uploadTaskfile(data){
     return this.http.post("/platform-service/document/uploadResource", data);
   }
