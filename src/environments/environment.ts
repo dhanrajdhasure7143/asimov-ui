32e1892f892c29e008bbd1020ee7a844cb702660
Deepak Pilla <deepak.pilla@epsoftinc.com>
2021-08-04 18:48:03 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,72 +6,74 @@ export const environment = {
   production: false,
 
   // Data upload path
-   data_path:"/var/kafka",
+  data_path:"/var/kafka",
 
-//   //  * DEV ENV URl's
+  //  * DEV ENV URl's
 
-  pi_url: 'http://pidev.epsoftinc.in',
+  //pi_url: 'http://pidev.epsoftinc.in',
 
-  bps_url: 'http://bpsdev.epsoftinc.in',
-  rpa_url: 'http://rpadev.epsoftinc.in',
-  loopback_url: 'http://10.11.0.107:3000',
-  idm_url:'http://idmdev.epsoftinc.in',
-  auth_url: "http://authdev.epsoftinc.in",
+  //bps_url: 'http://bpsdev.epsoftinc.in',
+  //rpa_url: 'http://rpadev.epsoftinc.in',
+  //loopback_url: 'http://10.11.0.107:3000',
+  //idm_url:'http://idmdev.epsoftinc.in',
+  //auth_url: "http://authdev.epsoftinc.in",
   //for Auto build
-  pi_node_url: "http://10.11.0.107:3000",
-  pi_node_jobs: "http://10.11.0.107:5000",
+  //pi_node_url: "http://10.11.0.107:3000",
   //for local environment
   //pi_node_url: "http://10.11.0.112:3000",
+  
+  //platform_url:'http://apdev.epsoftinc.in',
+  //schema_registry_url:"http://10.11.0.101:8081",
+  //alerts_url:'http://alertsdev.epsoftinc.in',
 
-  platform_url:'http://apdev.epsoftinc.in',
-  schema_registry_url:"http://10.11.0.101:8081",
-  alerts_url:'http://alertsdev.epsoftinc.in',
-
- // Aiotal redirection urls
-  myaccount_url : "http://eiapclouddev.epsoftinc.in/#/activation",
-  logout_url: 'http://eiapclouddev.epsoftinc.in/#/activation',
-  redirectout_url: 'http://eiapclouddev.epsoftinc.in/#/signout',
-  platform_home_url: 'http://eiapclouddev.epsoftinc.in/#/',
-  projectendpoint_url:'http://10.11.0.107:8282',
+ // Aiotal redirection urls 
+  //myaccount_url : "http://eiapclouddev.epsoftinc.in/#/activation",
+  //logout_url: 'http://eiapclouddev.epsoftinc.in/#/activation', 
 
 // PI Connectors
-  connector_topic: 'nodeDev',
+  //connector_topic: 'nodeDev',
 
 //BPMN Platform URL
-  bpmnplatformUrl : "http://10.11.0.127:8080",
+ // bpmnplatformUrl : "http://10.11.0.127:8080",
 
 //DB Connector
-  dbConnectorHost: "10.11.0.113",
-  dbName: "asimov_aiotal",
+  //dbConnectorHost: "10.11.0.113",
+  //dbName: "asimov_aiotal",
+
 
   //  * QA ENV URl's
-  //   pi_url: 'http://piqa.epsoftinc.in',
-  //   bps_url: 'http://bpsqa.epsoftinc.in',
-  //   rpa_url: 'http://rpaqa.epsoftinc.in',
-  //   loopback_url: 'http://10.11.0.107:3000',
-  //   idm_url:'http://10.11.0.108:8888',
-  //   pi_node_url: "http://10.11.0.108:3000",
-  //     pi_node_jobs: "http://10.11.0.108:5000",
-  //   auth_url: "http://authqa.epsoftinc.in",
-  //   platform_url:'http://apqa.epsoftinc.in',
-  //   schema_registry_url:"http://10.11.0.101:8081",
-  //   alerts_url:'http://alertsqa.epsoftinc.in',
+  
+     pi_url: 'http://piqa.epsoftinc.in',
+     bps_url: 'http://bpsqa.epsoftinc.in',
+     rpa_url: 'http://rpaqa.epsoftinc.in',
+     loopback_url: 'http://10.11.0.107:3000',
+     idm_url:'http://idmqa.epsoftinc.in',
+     pi_node_url: "http://10.11.0.108:3000",
+     auth_url: "http://authqa.epsoftinc.in",
+     platform_url:'http://apqa.epsoftinc.in',
+     schema_registry_url:"http://10.11.0.101:8081",
+     alerts_url:'http://alertsqa.epsoftinc.in',
+     pi_node_jobs: "http://10.11.0.108:5000",
+    
 
-  // // Aiotal redirection urls
-  //  myaccount_url : "http://eiapcloudqa.epsoftinc.in/#/activation",
-  //  logout_url: 'http://eiapcloudqa.epsoftinc.in/#/activation',
-  // redirectout_url: 'http://eiapcloudqa.epsoftinc.in/#/signout',
-  // platform_home_url: 'http://eiapcloudqa.epsoftinc.in/#/',
+  // Aiotal redirection urls 
+    myaccount_url : "http://eiapcloudqa.epsoftinc.in/#/activation",
+    logout_url: 'http://eiapcloudqa.epsoftinc.in/#/activation',
+    redirectout_url: 'http://eiapcloudqa.epsoftinc.in/#/signout',
+    platform_home_url: 'http://eiapcloudqa.epsoftinc.in/#/',
 
-  // // PI Connectors
-  //  connector_topic: 'nodeq',
+    // PI Connectors
+    connector_topic: 'nodeq',
 
-  // //DB Connector
-  //  dbConnectorHost: "10.11.0.104",
-  //  dbName: 'eiap_qa',
 
-  // // BPMN Platform URL
-  //   bpmnplatformUrl : "http://10.11.0.127:8080",
+   //DB Connector
+    dbConnectorHost: "10.11.0.104",
+    dbName: 'eiap_qa',
+
+  
+  //BPMN Platform URL
+  bpmnplatformUrl : "http://10.11.0.128:8080"
+
 };
 
 /*
@@ -82,3 +84,5 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
