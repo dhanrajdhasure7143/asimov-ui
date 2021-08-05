dbf4abac6ffebe50b33dd06878d4af63008113bb
srikanth.pothina <srikanth.pothina@epsoftinc.com>
2021-08-05 18:24:19 +0530

    bps-approvalworkflow-uichanges

--- src/app/pages/approval-workflow/approval-workflow.component.ts
+++ src/app/pages/approval-workflow/approval-workflow.component.ts
@@ -5,7 +5,10 @@ import { Component, OnInit } from '@angular/core';
   template: `<div class="main-content">
               <div class="row content-area">
                 <div class=" row module-heading title">
-                  <span class="module-heading-title">Business Process Studio</span>
+                  <span class="module-back-button" routerLink="/home">
+                    <i class="fas fa-arrow-left" aria-hidden="true"></i>
+                  </span>
+                  <span class="module-heading-title">Business Process Studio / Approval Workflow</span>
                 </div>
                 <router-outlet></router-outlet>
               </div>
