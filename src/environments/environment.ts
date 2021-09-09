da7735efb5dc793c603d81ab49a61303784d0155
Deepak Pilla <deepak.pilla@epsoftinc.com>
2021-09-09 16:30:17 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -24,6 +24,7 @@ export const environment = {
   connector_topic: 'nodeq',
   platform_home_url: 'https://ezflow.epsoftinc.com/#/',
   pi_node_jobs: "http://pi-node-jobs:5000",
+  projectendpoint_url:'httpshttps://ezflow.epsoftinc.com/aiotalplatform/pcs',
 
 
 //BPMN Platform URL
