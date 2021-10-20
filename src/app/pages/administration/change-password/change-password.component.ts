fc9be079c0a6cb3664ec72eeec17b894c88ac303
Aditya Vardana Sandilya Jandyala <aditya.jandhyala@epsoftinc.global>
2021-10-20 13:41:54 +0530

    [PLATFORM-1139] Added new components for user management and myaccount

---
+++ src/app/pages/administration/change-password/change-password.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { RestApiService } from '../../services/rest-api.service';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent implements OnInit {
+  pswdmodel:any = {};
+  public eyeshow: boolean = true;
+  public neweyeshow: boolean = true;
+  public confeyeshow: boolean = true;
+
+  constructor( private api:RestApiService,) { }
+
+  ngOnInit(): void {
+  }
+
+  passwordChange(form:NgForm){
+    let pswdbody = {
+      "confirmPassword": this.pswdmodel.confirmPassword,
+      "currentPassword": this.pswdmodel.currentPassword,
+      "newPassword":this.pswdmodel.confirmPassword,
+      "userId": localStorage.getItem('ProfileuserId')
+    }
+  this.api.changePassword(pswdbody).subscribe(res => {
+  // this.pswdmodel = {};
+  if(res.errorMessage === undefined){
+    Swal.fire({
+      title: "Success",
+      text: "Password Updated successfully!",
+      position: 'center',
+      icon: 'success',
+      showCancelButton: false,
+      confirmButtonColor: '#007bff',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Ok'
+    });}else if(res.errorMessage === "Your current password was incorrect."){
+      Swal.fire("Error","Please check your current password!","error");
+     
+    }else if(res.errorMessage === "The new password must be different from your previous used passwords"){
+      Swal.fire("Error",res.errorMessage,"error");
+     
+    }
+  }, err => {
+    // console
+    Swal.fire("Error","Please check your current password!","error");})
+ form.resetForm();
+  }
+
+  curreyetoggle() {
+    this.eyeshow = !this.eyeshow;
+  }
+  neweyetoggle() {
+    this.neweyeshow = !this.neweyeshow;
+  }
+  confeyetoggle() {
+    this.confeyeshow = !this.confeyeshow;
+  }
+
+}
