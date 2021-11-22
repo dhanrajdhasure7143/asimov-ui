578f8f2932db9961a7709e4f97b6ad48f7910b5b
Bhavya Krishna Kavuri <bhavya.kavuri@epsoftinc.global>
2021-11-22 08:37:37 +0530

    [RPA-1018] Add and delete tasks in a process

--- src/app/pages/service-orchestration/service-orchestration.module.ts
+++ src/app/pages/service-orchestration/service-orchestration.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxSpinnerModule } from "ngx-spinner";
+import { NgSelectModule } from '@ng-select/ng-select';
 import { SharedModule } from '../../shared/shared.module';
 import { ServiceOrchestrationRoutingModule } from './service-orchestration-routing.module';
 import { ServiceOrchestrationComponent } from './service-orchestration.component';
@@ -117,6 +118,7 @@ import { CheckResourcePipe } from './orchestration/new-so-automated-tasks/check-
     MatDialogModule,
     NgxPaginationModule,
     ModalModule.forRoot(),
+    NgSelectModule
 
   ],
   bootstrap: [SoDashboardComponent],
