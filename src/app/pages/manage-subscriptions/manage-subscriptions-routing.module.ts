15865ac34097b48427bde35493ddf0ff809aa23d
Vishala Thallpally <vishala.thallapally@epsoftinc.global>
2022-01-24 16:32:22 +0530

    [platform-1264]

--- src/app/pages/manage-subscriptions/manage-subscriptions-routing.module.ts
+++ src/app/pages/manage-subscriptions/manage-subscriptions-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CurrentplanComponent } from './currentplan/currentplan.component';
 import { ManageSubscriptionsComponent } from './manage-subscriptions.component';
+import { AddcardComponent } from './addcard/addcard.component';
 
 
 const routes: Routes = [
-  {path:'', component:ManageSubscriptionsComponent}
+  {path:'', component:ManageSubscriptionsComponent},
+  {path:'addcard',component:AddcardComponent}
 ];
 
 @NgModule({
