640e07217c6c4ed51afb9d047a73ebc29b9cd771
praveen.bokkala <praveen.bokkala@epsoftinc.com>
2022-06-05 19:54:00 +0530

    UI done for RPA Design Screen

--- src/app/pages/projects/project-rpa-design/project-rpa-design.component.ts
+++ src/app/pages/projects/project-rpa-design/project-rpa-design.component.ts
@@ -5,6 +5,21 @@ import { MatTableDataSource } from '@angular/material/table';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { RestApiService } from '../../services/rest-api.service';
 
+export interface data {
+  steps: string;
+  description: string;
+  configuration: string;
+  isEdit: boolean;
+  }
+
+const COLUMNS_SCHEMA = [
+  {key: 'sNo',label: 'S No',type:'number',},
+  {key: 'steps',label: 'Steps',},
+  {key: 'description',label: 'Description',},
+  {key: 'configuration',label: 'Configuration',},
+  {key: 'isEdit',type: 'isEdit',label: 'Action'},
+];
+
 @Component({
   selector: 'app-project-rpa-design',
   templateUrl: './project-rpa-design.component.html',
@@ -15,23 +30,53 @@ export class ProjectRpaDesignComponent implements OnInit {
   @ViewChild("paginator", { static: false }) paginator: MatPaginator;
   data:any=[];
 
-  displayedColumns: string[] = ['sNo', 'steps', 'description', 'configuration',  'Action'];
+  // displayedColumns: string[] = ['sNo', 'steps', 'description', 'configuration',  'Action'];
 
   dataSource: MatTableDataSource<any[]>;
+  displayedColumns: string[] = COLUMNS_SCHEMA.map((col) => col.key);
+  columnsSchema: any = COLUMNS_SCHEMA;
+  USER_DATA :data[]= [
+    {steps:"Login to Satuit",description:"Testing Description", configuration:"a.Testing",isEdit:false},
+    {steps:"Download",description:"This action is apply search  criteria and export the result to excel", configuration:"a.HR",isEdit:false},
+    {steps:"Login to seismic",description:"Testing 123", configuration:"a.Client ID",isEdit:false},
+    {steps:"Tetsing",description:" DEscription", configuration:"a.User",isEdit:false}
+  ];
+  public myDataArray: any;
+
+  public newUser ={steps:"",description:"", configuration:"",isEdit:true};
 
   constructor() { }
 
   ngOnInit(): void {
-    this.data = [
-      {steps:"Login to Satuit",description:"Testing Description", configuration:"a.testing"}
-    ]
-    this.dataSource = new MatTableDataSource(this.data);
+    this.myDataArray = [...this.USER_DATA];
+    // this.dataSource = new MatTableDataSource(this.data);
   }
 
   onAddRow(){
-    let data1 =  {steps:"",description:"", configuration:""}
+    let data1 =  {steps:"",description:"", configuration:"",new:'true'}
     this.data.push(data1)
-    this.dataSource = new MatTableDataSource(this.data);
+    // this.dataSource = new MatTableDataSource(this.data);
   }
+
+  addUser() {
+    let newUser1 ={steps:"",description:"", configuration:"",isEdit:true};
+    this.USER_DATA.splice(0,0,newUser1)
+    const newUsersArray = this.USER_DATA;
+    // newUsersArray.push(newUser1);
+    this.myDataArray = [...newUsersArray];
+    }
+
+  saveTableRow(e){
+    console.log(e)
+  }
+
+  deleteNewRow(index){
+    console.log(index,this.USER_DATA)
+    // this.USER_DATA = this.USER_DATA.filter((value,key)=>{
+    // return value.email != row_obj.email;
+    // });
+    // this.myDataArray = [...this.USER_DATA];//refresh the dataSource
+    // Swal.fire('Deleted successfully..!')
+    }
 
 }
