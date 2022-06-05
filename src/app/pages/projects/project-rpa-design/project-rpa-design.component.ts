641f91a60714269150e93953c44de3972369558f
praveen.bokkala <praveen.bokkala@epsoftinc.com>
2022-06-05 11:29:40 +0530

    input Fields added in table

--- src/app/pages/projects/project-rpa-design/project-rpa-design.component.ts
+++ src/app/pages/projects/project-rpa-design/project-rpa-design.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { RestApiService } from '../../services/rest-api.service';
 
 @Component({
   selector: 'app-project-rpa-design',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-rpa-design.component.css']
 })
 export class ProjectRpaDesignComponent implements OnInit {
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild("paginator", { static: false }) paginator: MatPaginator;
+  data:any=[];
+
+  displayedColumns: string[] = ['sNo', 'steps', 'description', 'configuration',  'Action'];
+
+  dataSource: MatTableDataSource<any[]>;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.data = [
+      {steps:"Login to Satuit",description:"Testing Description", configuration:"a.testing"}
+    ]
+    this.dataSource = new MatTableDataSource(this.data);
+  }
+
+  onAddRow(){
+    let data1 =  {steps:"",description:"", configuration:""}
+    this.data.push(data1)
+    this.dataSource = new MatTableDataSource(this.data);
   }
 
 }
