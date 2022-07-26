5bc11135d09e92813bd1ac90e2123e36a2dd5c36
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2022-07-26 21:23:35 +0530

    drowndown loading issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserIdleService } from 'angular-user-idle';
 import { APP_CONFIG } from './app.config';
@@ -16,7 +16,7 @@ export class AppComponent {
   newAccessToken: any;
   constructor(private userIdle: UserIdleService, private apiservice: RestApiService,
      private authservice: AuthenticationService, @Inject(APP_CONFIG) private config,
-     private router: Router,private toastr: ToastrService, private cdRef: ChangeDetectorRef) { }
+     private router: Router,private toastr: ToastrService) { }
 
   ngOnInit() {
     addEventListener("offline",(e)=>{
@@ -63,14 +63,6 @@ export class AppComponent {
  
   restart() {
     this.userIdle.resetTimer();
-  }
-
-  ngAfterViewChecked() {
-    setTimeout(() => {
-      if(!localStorage.getItem("accessToken"))
-      this.router.navigate(['/redirect'])
-    }, 6000);
-    this.cdRef.detectChanges();
   }
 
 }
