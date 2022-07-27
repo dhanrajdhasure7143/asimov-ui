7b16dc1bea66a4bb3c3b814dc73b82d4bf29c2f2
vishala.thallapally <vishala.thallapally@epsoftinc.com>
2022-07-27 16:21:25 +0530

    code changes done for EQD-705

--- src/app/pages/business-process/home/home.component.ts
+++ src/app/pages/business-process/home/home.component.ts
@@ -145,11 +145,14 @@ export class BpsHomeComponent implements OnInit {
       this.assignPagenation(this.saved_diagrams);
 
       let selected_category=localStorage.getItem("bps_search_category");
-      if(this.categories_list.length == 1){
-        this.categoryName=this.categories_list[0].categoryName;
-      }else{
-        this.categoryName=selected_category?selected_category:'allcategories';
-      }
+      setTimeout(() => {
+        if(this.categories_list.length == 1){
+          this.categoryName=this.categories_list[0].categoryName;
+        }else{
+          this.categoryName=selected_category?selected_category:'allcategories';
+        }
+      }, 1000);
+     
       this.searchByCategory(this.categoryName);
     },
     
