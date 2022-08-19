b219dbb2397d051bc5c0614f3ef47ccbb68ddaf2
vishala.thallapally <vishala.thallapally@epsoftinc.com>
2022-08-19 10:30:42 +0530

    changes for EQD-732

--- src/app/pages/rpautomation/rpa-scheduler/rpa-scheduler.component.ts
+++ src/app/pages/rpautomation/rpa-scheduler/rpa-scheduler.component.ts
@@ -3,8 +3,11 @@ import { CronOptions } from 'src/app/shared/cron-editor/CronOptions';
 import {RestApiService} from 'src/app/pages/services/rest-api.service';
 import Swal from 'sweetalert2';
 import cronstrue from 'cronstrue';
+import moment from 'moment';
 import { NotifierService } from 'angular-notifier';
 import { RpaStudioActionsmenuComponent } from '../rpa-studio-actionsmenu/rpa-studio-actionsmenu.component'
+import { first } from 'rxjs/operators';
+import { any } from '@amcharts/amcharts4/.internal/core/utils/Array';
 
 @Component({
   selector: 'app-rpa-scheduler',
@@ -17,21 +20,10 @@ export class RpaSchedulerComponent implements OnInit {
   @Input('data') public data: any;
   botid:any;
   processid:any;
+  beforetime:boolean=false;
   public Environments:any;
-  public timesZones: any[] = [
-    "America/New_York",
-    "America/Chicago",
-    "America/Los_Angeles",
-    "America/Denver",
-    "Canada/Atlantic",
-    "Canada/Central",
-    "Canada/Eastern",
-    "Canada/Mountain",
-    "Asia/Dubai",
-    "Asia/Calcutta",
-    "UTC",
-    "GMT",
-  ];
+  public timesZones: any = ["UTC","Asia/Dubai","America/New_York","America/Los_Angeles","Asia/Kolkata","Canada/Atlantic","Canada/Central","Canada/Eastern","GMT"];
+  //public timesZones: any = [];
   i="";
   public cronOptions: CronOptions = {
     formInputClass: 'form-control cron-editor-input',
@@ -63,6 +55,8 @@ export class RpaSchedulerComponent implements OnInit {
   picker1;
   picker2;
   starttime:any;
+  h:any;
+  todaytime:any;
   endtime:any="23:59";
   schedules:any=[];
   startdate:any= new Date();
@@ -73,6 +67,12 @@ export class RpaSchedulerComponent implements OnInit {
   selectedEnvironment:any;
   environmentid:any;
   deletestack:any=[];
+  month:any;
+  day:any;
+  endtimeerror:any;
+  issame:boolean;
+  selecteddate:any;
+  isbefore:boolean;
   flags={
     startflag:false,
     stopflag:false,
@@ -81,12 +81,29 @@ export class RpaSchedulerComponent implements OnInit {
     deleteflag:false,
   }
   q=0;
+  currenttime: any;
+  start_time:any;
+  end_time:any;
+  starttimeerror:any;
+  aftertime:boolean=false;
   constructor(private rest:RestApiService, private notifier: NotifierService, private actions:RpaStudioActionsmenuComponent) { }
 
   ngOnInit() {
-    this.startdate=this.startdate.getFullYear()+"-"+(this.startdate.getMonth()+1)+"-"+this.startdate.getDate();
-    this.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    this.onTimeZoneChange(this.timezone);
+    
+    var dtToday = new Date();
+    this.selecteddate=new Date()
+   // this.startdate=this.startdate.getFullYear()+"-"+(this.startdate.getMonth()+1)+"-"+this.startdate.getDate();
+   this.month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(this.month < 10)
+        this.month = '0' + this.month.toString();
+    if(day < 10)
+        this.day = '0' + day.toString();
+    
+    var minDate= year + '-' + this.month + '-' + day;
+    $('#txtDate').attr('min', minDate);
+    $('#enddatepicker').attr('min', minDate);
     if(this.data.botid!=undefined && this.data.botid !="not_saved")
     {
       this.botid=this.data.botid;
@@ -99,10 +116,34 @@ export class RpaSchedulerComponent implements OnInit {
       this.schedule_list=this.data.schedule_list;
     }
     this.enddate=this.startdate;
+   this.gettime();
+
+    console.log("todaytime",this.todaytime);
+
     this.starttime=(new Date).getHours()+":"+(new Date).getMinutes();
-
-  }
-
+    // this.getAlltimezones();
+  }  
+gettime(){
+ 
+  this.todaytime=(new Date).getHours()+":"+(new Date).getMinutes();
+  // this.todaytime="8:6"
+   let firstchar=this.todaytime.split(":")
+   let str1='0'
+   if(firstchar[0]<10){
+     var firstchar1=str1.concat(firstchar[0])
+     
+   }
+   else{
+     firstchar1=firstchar[0]
+   }
+   if(firstchar[1]<10){
+     var firstchar2=str1.concat(firstchar[1])
+   }
+   else{
+     firstchar2=firstchar[1]
+   }
+   this.todaytime=firstchar1+ ":" +firstchar2
+}
   get_schedule()
   {
     this.schedule_list=[];
@@ -146,12 +187,140 @@ export class RpaSchedulerComponent implements OnInit {
     this.enddate=this.startdate;
     this.starttime=d.getHours()+":"+d.getMinutes();
   }
-  dateChange($event,date){
-   
-   if(date=='startdate')
-   this.enddate=this.startdate;
-   if(this.isDateToday($event.target.value)) {
+  onChangeHour(event,time){ 
+ 
+ this.todaytime=(new Date).getHours()+":"+(new Date).getMinutes();;
+ 
+    event=this.tConv24(event)
+    this.beforetime=false;
+    this.aftertime=false;
+    
+    if(this.isDateToday(this.selecteddate)){
+      if(time=='starttime'){
+     
 
+
+        this.currenttime=this.tConv24(this.todaytime)
+        this.end_time=this.tConv24(this.endtime)
+         let a=moment(event,'h:mma')
+         let b=moment(this.currenttime,'h:mma')
+         let f=moment(this.end_time,'h:mma')
+       
+         this.isbefore=(a.isBefore(b));
+         let g=(a.isAfter(f))
+         this.issame=(a.isSame(f))
+        if(this.isbefore){
+          this.starttimeerror="start time should not be before than current time"
+          this.beforetime=true
+        }
+        
+        if(g){
+           this.starttimeerror="start time should not be greater than end time";
+           this.beforetime=true
+        }
+        if(this.issame){
+          this.starttimeerror="start time should not be equal to end time";
+          this.beforetime=true
+        }
+       
+      }
+      else{
+        this.start_time=this.tConv24(this.starttime);
+        this.currenttime=this.tConv24(this.todaytime)
+        
+        let c=moment(this.start_time,'h:mma')
+        let d=moment(event,'h:mma');
+        let currenttime=moment(this.currenttime,'h:mma')
+        let beforecurrenttime=(d.isBefore(currenttime))
+       
+        let e=(c.isBefore(d))
+       let starttime_error=(c.isBefore(currenttime))
+        console.log(e)
+        if(e==false ){
+          this.aftertime=true;
+          this.endtimeerror="end time should not be before than or equal to start time"
+        }
+        if(beforecurrenttime){
+          this.aftertime=true;
+          this.endtimeerror="end time should not be before than or equal to currenttime"
+        }
+       if(starttime_error){
+         this.beforetime=true;
+         this.starttimeerror="start time should not be before than current time"
+       }
+      }
+    }
+    else{
+      if(this.startdate==this.enddate){
+        if(time=='starttime'){
+     
+
+          this.end_time=this.tConv24(this.endtime)
+          this.start_time=this.tConv24(this.starttime)
+           let a=moment(this.end_time,'h:mma')
+           let b=moment(this.start_time,'h:mma')
+          this.issame=(a.isSame(b))
+           this.isbefore=(b.isAfter(a));
+          if(this.isbefore){
+            this.beforetime=true;
+            this.starttimeerror="start time should not be before than end time"
+  
+          }
+          if(this.issame ){
+            this.beforetime=true;
+            this.starttimeerror="start time should not be equal to end time"
+          }
+         
+        }
+        else{
+          this.end_time=this.tConv24(this.endtime)
+          this.start_time=this.tConv24(this.starttime)
+           let a=moment(this.end_time,'h:mma')
+           let b=moment(this.start_time,'h:mma')
+           this.issame=(a.isSame(b))
+           this.isbefore=(a.isBefore(b));
+           if(this.isbefore ){
+            this.aftertime=true;
+            this.endtimeerror="end time should not be before than start time"
+           }
+           if(this.issame){
+            this.aftertime=true;
+            this.endtimeerror="end time should not be equal to start time"
+           }
+          
+  
+        }
+      }
+     
+    }
+   
+   
+  }
+  tConv24(time24) {
+    
+    
+    var ts = time24;
+    var H = +ts.substr(0, 2);
+    this.h = (H % 12) || 12;
+    this.h = (this.h < 10)?("0"+this.h):this.h;  // leading 0 at the left for 1 digit hours
+    var ampm = H < 12 ? " AM" : " PM"; 
+    ts = this.h + ts.substr(2, 3) +ampm;
+    return ts;
+
+  };
+
+  dateChange($event,date){
+    this.beforetime=false;
+    this.aftertime=false;
+   if(date=='startdate'){
+    this.enddate=this.startdate;
+    $('#enddatepicker').attr('min', this.startdate);
+   }
+  
+   this.selecteddate=$event.target.value
+   if(this.isDateToday($event.target.value)) {
+    this.starttime=(new Date).getHours()+":"+(new Date).getMinutes();
+    this.endtime='23:59'
    }
     else{
       this.starttime="00:00";
@@ -175,7 +344,37 @@ export class RpaSchedulerComponent implements OnInit {
   }
   add_sch()
   {
+    
     // Scheduler
+    if(this.isDateToday(this.selecteddate)){
+      this.todaytime=(new Date).getHours()+":"+(new Date).getMinutes();;
+      let current_time=this.tConv24(this.todaytime)
+      let start_time=this.tConv24(this.starttime)
+       let validatecurrenttime=moment(start_time,'h:mma');
+       let validatesystemtime=moment(current_time,'h:mma');
+       let isbefore=(validatecurrenttime.isBefore(validatesystemtime));
+       if(isbefore){
+         this.beforetime=true;
+         this.starttimeerror="start time should not be before than current time"
+       }
+       else{
+         this.beforetime=false
+        this.addscheduler()
+       }
+       
+    }
+    else{
+      this.addscheduler()
+    }
+   
+
+   
+   
+ 
+
+  }
+
+  addscheduler(){
     if(this.startdate !="" && this.enddate!=""  && this.cronExpression != "" && this.starttime!=undefined && this.endtime!=undefined && this.timezone!="" && this.timezone!=undefined)
     {
       let starttime=this.starttime.split(":")
@@ -192,8 +391,7 @@ export class RpaSchedulerComponent implements OnInit {
           scheduleInterval:this.cronExpression,
           startDate:parseInt(startdate[0])+","+parseInt(startdate[1])+","+parseInt(startdate[2])+","+starttimeparse+","+starttime[1],
           endDate:parseInt(enddate[0])+","+parseInt(enddate[1])+","+parseInt(enddate[2])+","+ endtimeparse+","+ endtime[1],
-          // startDate:this.startdate.getFullYear()+","+(this.startdate.getMonth()+1)+","+this.startdate.getDate()+","+starttime[0]+","+starttime[1],
-          // endDate:this.enddate.getFullYear()+","+(this.enddate.getMonth()+1)+","+this.enddate.getDate()+","+ endtime[0]+","+ endtime[1],
+         
         
           timeZone:this.timezone,
           save_status:"unsaved",
@@ -206,9 +404,8 @@ export class RpaSchedulerComponent implements OnInit {
     {
 
       this.notifier.notify("error", "Please fill all inputs");
-      //Swal.fire("Please fill all details","","error");
+     
     }
-
   }
 
   check_all(event)
@@ -249,8 +446,7 @@ export class RpaSchedulerComponent implements OnInit {
         {
          
           this.notifier.notify("success",resp.status)
-          // this.schedule_list.find(data=>data.check==true).run_status="started";
-          // this.updateflags();
+        
           this.get_schedule();
         }
         else
@@ -279,8 +475,7 @@ export class RpaSchedulerComponent implements OnInit {
         if(resp.errorMessage==undefined)
         {
           this.notifier.notify("success",resp.status)
-          // this.schedule_list.find(data=>data.check==true).run_status="pause";
-          // this.updateflags();
+        
           this.get_schedule();
         }
         else
@@ -306,8 +501,6 @@ export class RpaSchedulerComponent implements OnInit {
       if(resp.errorMessage==undefined)
       {
         this.notifier.notify("success", resp.status);
-        // this.schedule_list.find(data=>data.check==true).run_status="resume";
-        // this.updateflags();
         this.get_schedule();
       }
       else
@@ -335,72 +528,7 @@ export class RpaSchedulerComponent implements OnInit {
 
 
 
-  // async saveschedule()
-  // {
-  //   if(this.botid !=undefined && this.botid != "" && this.botid!="not_saved")
-  //   {
-  //     if(this.schedule_list.length==0)
-  //     {
-  //       this.botdata.botMainSchedulerEntity=null;
-  //     }
-  //     else
-  //     {
-  //       let schedules:any=[]
-  //       this.schedule_list.forEach(data=>{
-  //         if(data.save_status=="unsaved")
-  //         {
-  //           delete data.intervalId
-  //           schedules.push(data);
-  //         }
-  //         else if(data.save_status=="saved")
-  //         {
-  //           schedules.push(data)
-  //         }
-  //       })
-  //       if(this.botdata.botMainSchedulerEntity==null)
-  //       {
-  //         this.botdata.botMainSchedulerEntity={"scheduleIntervals":schedules};
-  //       }
-  //       else if(schedules.length==0)
-  //       {
-  //         this.botdata.botMainSchedulerEntity=null;
-  //       }
-  //     }
-  //     await (await this.rest.updateBot(this.botdata)).subscribe(data =>{
-  //       let resp:any=data;
-  //       if(resp.botMainSchedulerEntity.scheduleIntervals.length==0)
-  //       {
-  //         this.notifier.notify("success","Updated successfully")
-  //       }
-  //       else if(resp.botMainSchedulerEntity.scheduleIntervals.length==this.schedule_list.length)
-  //       {
-  //         //this.actions.schpop=false;
-  //         //this.close()
-
-  //         this.notifier.notify("success", "Schedules saved successfully");
-  //         //Swal.fire("Schedules saved successfully","","success");
-  //       }
-  //       this.get_schedule();
-  //     })
-  //   }
-  //   else
-  //   {
-  //     let schedules:any=[]
-  //       this.schedule_list.forEach(data=>{
-  //         if(data.save_status=="unsaved")
-  //         {
-  //           delete data.intervalId
-  //           schedules.push(data);
-  //         }
-  //       });
-  //     this.notifier.notify("success","Schedule configured successfully")
-  //     let sch:any={
-  //       scheduleIntervals:schedules,
-  //     }
-  //     this.actions.saveschedule(sch,this.schedule_list);
-  //   }
-  // }
-
+  
   async saveschedule()
   {
     if(this.botid !=undefined && this.botid != "" && this.botid!="not_saved")
@@ -443,11 +571,10 @@ export class RpaSchedulerComponent implements OnInit {
         }
         else if(resp.botMainSchedulerEntity.scheduleIntervals.length==this.schedule_list.length)
         {
-          //this.actions.schpop=false;
-          //this.close()
+        
 
           this.notifier.notify("success", "Schedules saved successfully");
-          //Swal.fire("Schedules saved successfully","","success");
+          
         }
        
         this.get_schedule();
@@ -565,6 +692,7 @@ export class RpaSchedulerComponent implements OnInit {
     this.selectedEnvironment="";
     this.timezone="";
   }
+  
 
 }
 
