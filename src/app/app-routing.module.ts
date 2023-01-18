a7064972db5d1dcd06b05e9171e4f7666813cc13
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-01-18 18:16:58 +0530

    CLI UPDATE TO 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path:'', redirectTo:"login", pathMatch:"full" },
   { path: 'login', component: LoginComponent },
   { path: 'redirect', component: RedirectionComponent },
-  { path: 'pages', loadChildren: 'src/app/pages/pages.module#PagesModule', canActivate: [AuthGuard] },//resolve: {userSharedData: ContentReslover}
+  { path: 'pages', loadChildren: () => import('src/app/pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },//resolve: {userSharedData: ContentReslover}
   { path: '**', redirectTo: 'login' }
 ];
 
