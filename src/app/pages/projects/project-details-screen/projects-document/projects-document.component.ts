6bbec75fb49bfbf76462413efea569ea59db0a24
sai.gangala <sai.gangala@epsoftinc.com>
2023-02-02 19:18:15 +0530

    Prime NG Tree View

--- src/app/pages/projects/project-details-screen/projects-document/projects-document.component.ts
+++ src/app/pages/projects/project-details-screen/projects-document/projects-document.component.ts
@@ -11,7 +11,17 @@ export class ProjectsDocumentComponent implements OnInit {
   display: boolean = false;
   isSidebar: boolean = false;
   isDialog: boolean = false;
-  folder: any;
+  entered_folder_name: any;
+  selectedFile: TreeNode;
+  text: string;
+  folder_name: any;
+  isDialogBox: boolean = false;
+  example: TreeNode = {
+    label: "example",
+    data: "Movies Folder",
+    expandedIcon: "pi pi-folder-open",
+    collapsedIcon: "pi pi-folder-open",
+  };
 
   constructor() {}
 
@@ -46,6 +56,12 @@ export class ProjectsDocumentComponent implements OnInit {
         expandedIcon: "pi pi-folder-open",
         collapsedIcon: "pi pi-folder",
         children: [
+          {
+            label: "Add Folder / Document",
+            data: "Work Folder",
+            expandedIcon: "pi pi-folder-open",
+            collapsedIcon: "pi pi-folder",
+          },
           { label: "Document 1", icon: "pi pi-file", data: "Document" },
           { label: "Document 2", icon: "pi pi-file", data: "Document" },
           { label: "Document 3", icon: "pi pi-file", data: "Document" },
@@ -57,6 +73,12 @@ export class ProjectsDocumentComponent implements OnInit {
         expandedIcon: "pi pi-folder-open",
         collapsedIcon: "pi pi-folder",
         children: [
+          {
+            label: "Add Folder / Document",
+            data: "Work Folder",
+            expandedIcon: "pi pi-folder-open",
+            collapsedIcon: "pi pi-folder",
+          },
           { label: "Document 1", icon: "pi pi-file", data: "Document" },
           { label: "Document 2", icon: "pi pi-file", data: "Document" },
           { label: "Document 3", icon: "pi pi-file", data: "Document" },
@@ -68,6 +90,12 @@ export class ProjectsDocumentComponent implements OnInit {
         expandedIcon: "pi pi-folder-open",
         collapsedIcon: "pi pi-folder",
         children: [
+          {
+            label: "Add Folder / Document",
+            data: "Work Folder",
+            expandedIcon: "pi pi-folder-open",
+            collapsedIcon: "pi pi-folder",
+          },
           { label: "Document 1", icon: "pi pi-file", data: "Document" },
           { label: "Document 2", icon: "pi pi-file", data: "Document" },
           { label: "Document 3", icon: "pi pi-file", data: "Document" },
@@ -79,6 +107,12 @@ export class ProjectsDocumentComponent implements OnInit {
         expandedIcon: "pi pi-folder-open",
         collapsedIcon: "pi pi-folder",
         children: [
+          {
+            label: "Add Folder / Document",
+            data: "Work Folder",
+            expandedIcon: "pi pi-folder-open",
+            collapsedIcon: "pi pi-folder",
+          },
           { label: "Document 1", icon: "pi pi-file", data: "Document" },
           { label: "Document 2", icon: "pi pi-file", data: "Document" },
           { label: "Document 3", icon: "pi pi-file", data: "Document" },
@@ -96,14 +130,29 @@ export class ProjectsDocumentComponent implements OnInit {
     ];
   }
 
+  saveFolder() {
+    if (this.selectedFile && this.entered_folder_name) {
+      let object = { ...{}, ...this.example };
+      object.label = this.entered_folder_name;
+      this.selectedFile.children.push(object);
+      this.entered_folder_name = "";
+      console.log(
+        "Added child in ",
+        this.selectedFile,
+        "you can find in",
+        this.files
+      );
+      this.isDialog = false;
+    }
+  }
+
   nodeSelect(event) {
     if (event.node.label == "Add Folder") {
-      this.isSidebar = true;
+      this.isDialogBox = true;
     }
     for (let i = 0; i < 100; i++)
       if (event.node.children[i].label == "Add Folder / Document") {
-        this.display = true;
-        console.log(event);
+        this.isDialog = true;
       }
   }
 
@@ -111,7 +160,46 @@ export class ProjectsDocumentComponent implements OnInit {
     this.isDialog = true;
   }
 
-  saveFolder(event) {
-    console.log(event);
+  addParent() {
+    this.files.push({
+      label: this.folder_name,
+      data: "Movies Folder",
+      expandedIcon: "pi pi-folder-open",
+      collapsedIcon: "pi pi-folder-open",
+      children: [
+        {
+          label: "Add Folder / Document",
+          data: "Work Folder",
+          expandedIcon: "pi pi-folder-open",
+          collapsedIcon: "pi pi-folder",
+        },
+      ],
+    });
+    this.folder_name = "";
+    this.isDialogBox = false;
   }
+
+  // removeRoute(node) {
+  //   const parent: any = this.findById(this.files, node.parentId);
+  //   const index = parent.children.findIndex((c) => c.id === node.id);
+  //   console.log(index, 1);
+  //   parent.children.splice(index, 1);
+  // }
+  // public selected: any;
+
+  // findById(data, id) {
+  //   for (const node of data) {
+  //     if (node.id === id) {
+  //       return node;
+  //     }
+
+  //     if (node.children) {
+  //       const desiredNode = this.findById(node.children, id);
+  //       if (desiredNode) {
+  //         return desiredNode;
+  //       }
+  //     }
+  //   }
+  //   return false;
+  // }
 }
