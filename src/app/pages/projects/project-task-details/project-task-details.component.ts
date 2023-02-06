3ded7ea38a846218aa11a22cb1be6033320a208f
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-02-06 11:45:55 +0530

    taskdetails changes

--- src/app/pages/projects/project-task-details/project-task-details.component.ts
+++ src/app/pages/projects/project-task-details/project-task-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Inplace } from "primeng/inplace";
+import { LoaderService } from "src/app/services/loader/loader.service";
 import { DataTransferService } from "../../services/data-transfer.service";
 import { RestApiService } from "../../services/rest-api.service";
 
@@ -13,23 +14,25 @@ export class ProjectTaskDetailsComponent implements OnInit {
   @ViewChild("inplace") inplace!: Inplace;
   @ViewChild("inplace1") inplace1!: Inplace;
   @ViewChild("inplace2") inplace2!: Inplace;
-  
   desc: any =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque cumque quo fugiat mollitia quas id earum perferendis ratione repudiandae magni odio nulla eveniet rerum accusamus error, ducimus provident. Est.";
-  project_id:any;
-  task_details:any={};
-  users_list:any;
-  taskcomments_list:any[]=[]
-  taskhistory_list:any[]=[]
+  project_id: any;
+  task_details: any = {};
+  users_list: any[] = [];
+  taskcomments_list: any[] = [];
+  taskhistory_list: any[] = [];
 
-  constructor(private route : ActivatedRoute,
-    private rest_api : RestApiService,
-    private router: Router,private dataTransfer : DataTransferService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private rest_api: RestApiService,
+    private router: Router,
+    private dataTransfer: DataTransferService,
+    private spinner: LoaderService
+  ) {}
 
   ngOnInit(): void {
     this.getallusers();
   }
-
 
   inplaceActivate() {
     // this.ip.activate();
@@ -43,38 +46,38 @@ export class ProjectTaskDetailsComponent implements OnInit {
       let params: any = data;
       console.log(data);
       this.project_id = params.project_id;
-      this.rest_api.gettaskandComments(this.project_id).subscribe((response) => {
-        let taskList: any = response;
-        this.task_details = taskList.find((item) => item.id == data.task_id);
-        console.log(this.task_details)
-        this.taskcomments_list = this.task_details.comments;
-        this.taskhistory_list = this.task_details.history;
-      });
+      this.rest_api
+        .gettaskandComments(this.project_id)
+        .subscribe((response) => {
+          let taskList: any = response;
+          this.task_details = taskList.find((item) => item.id == data.task_id);
+          console.log(this.task_details);
+          this.taskcomments_list = this.task_details.comments;
+          this.taskhistory_list = this.task_details.history;
+        });
+      this.spinner.hide();
     });
   }
 
-  backToTaskList(){
-    console.log(this.project_id)
+  backToTaskList() {
+    console.log(this.project_id);
     // this.router.navigate(['/pages/projects/tasks'],{
     //   queryParams:{id: this.project_id}
     // })
   }
 
   getallusers() {
+    this.spinner.show();
     this.dataTransfer.tenantBased_UsersList.subscribe((res) => {
       if (res) {
         this.users_list = res;
         this.gettask();
       }
     });
-      // let user = this.users_list.find(
-      //   (item) => item.userId.userId == this.selectedtask.resources
-      // );
-      // this.taskresourceemail = user.userId.userId;
-      // this.getUserRole();
-
+    // let user = this.users_list.find(
+    //   (item) => item.userId.userId == this.selectedtask.resources
+    // );
+    // this.taskresourceemail = user.userId.userId;
+    // this.getUserRole();
   }
-
-  
-
 }
