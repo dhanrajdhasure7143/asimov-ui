13aa7e0834441e1a340716984e9b91a60104f19b
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-02-09 12:47:57 +0530

    task details edit changes

--- src/app/pages/projects/project-task-details/project-task-details.component.ts
+++ src/app/pages/projects/project-task-details/project-task-details.component.ts
@@ -8,7 +8,7 @@ import Swal from "sweetalert2";
 import * as moment from "moment";
 
 interface Status {
-  name: string,
+  name: string;
 }
 @Component({
   selector: "app-project-task-details",
@@ -21,33 +21,33 @@ export class ProjectTaskDetailsComponent implements OnInit {
   @ViewChild("inplace2") inplace2!: Inplace;
   @ViewChild("inplace3") inplace3!: Inplace;
   @ViewChild("inplace4") inplace4!: Inplace;
-  @ViewChild("inplace4") inplace5!: Inplace;
+  @ViewChild("inplace5") inplace5!: Inplace;
   task_desc: any;
   project_id: any;
-  project_name:any;
+  project_name: any;
   task_details: any = {};
   selected_task_details: any = {};
   users_list: any[] = [];
   taskcomments_list: any[] = [];
   taskhistory_list: any[] = [];
   date1: Date;
-  status: Status[];
-  attached_list:any=[];
-  checkBoxselected:any[]=[];
-  columns_list:any;
+  status_list: Status[];
+  attached_list: any = [];
+  checkBoxselected: any[] = [];
+  columns_list: any;
   selectedCity: Status;
-  taskName:any;
-  edit_task_field:any;
-  edit_resource_field:any;
-  edit_status_field:any;
-  hiddenPopUp:boolean = false;
-  isprojectCreateForm: boolean =false;
-  params_data:any;
-  due_date:any;
-  mindate:any;
-  add_comment : any;
-  added_comments_list:any=[];
-  edit_percentage_field:any;
+  taskName: any;
+  edit_task_field: any;
+  resources: any;
+  status: any;
+  hiddenPopUp: boolean = false;
+  isprojectCreateForm: boolean = false;
+  params_data: any;
+  endDate: any;
+  mindate: any;
+  add_comment: any;
+  added_comments_list: any = [];
+  percentageComplete: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -56,12 +56,13 @@ export class ProjectTaskDetailsComponent implements OnInit {
     private dataTransfer: DataTransferService,
     private spinner: LoaderService
   ) {
-    this.status = [
-    {name: 'New'},
-    {name: 'In Progress'},
-    {name: 'In Review'},
-    {name: 'Done'},
-];}
+    this.status_list = [
+      { name: "New" },
+      { name: "In Progress" },
+      { name: "In Review" },
+      { name: "Done" },
+    ];
+  }
 
   ngOnInit(): void {
     let today = new Date();
@@ -73,22 +74,25 @@ export class ProjectTaskDetailsComponent implements OnInit {
       this.params_data = data;
       this.project_id = this.params_data.project_id;
       this.project_name = this.params_data.project_name;
-      this.rest_api.getProjectTaskDetailsById(this.project_id,this.params_data.task_id)
+      this.rest_api
+        .getProjectTaskDetailsById(this.project_id, this.params_data.task_id)
         .subscribe((response) => {
-          console.log("testing",response)
+          console.log("testing", response);
           let taskList: any = response;
           this.task_details = taskList[0];
           // this.task_details = taskList.find((item) => item.id == data.task_id);
           // this.selected_task_details = taskList.find((item) => item.id == data.task_id);
-          console.log("taskDetails",this.task_details);
+          console.log("taskDetails", this.task_details);
           this.taskcomments_list = this.task_details.comments;
           this.taskhistory_list = this.task_details.history;
-          this.added_comments_list =[];
+          this.added_comments_list = [];
           // this.added_comments_list = this.task_details.history;
           this.task_desc = this.task_details.description;
 
           // this.due_date = moment(this.task_details.endDate).format("YYYY-MM-DD");
-        this.mindate = moment(this.task_details.startDate).format("YYYY-MM-DD");
+          this.mindate = moment(this.task_details.startDate).format(
+            "YYYY-MM-DD"
+          );
         });
       this.spinner.hide();
     });
@@ -99,8 +103,12 @@ export class ProjectTaskDetailsComponent implements OnInit {
     // this.router.navigate(['/pages/projects/tasks'],{
     //   queryParams:{id: this.project_id}
     // })
-  this.router.navigate(['/pages/projects/tasks'],{queryParams:{project_id:this.project_id,"project_name":this.project_name}});
-
+    this.router.navigate(["/pages/projects/tasks"], {
+      queryParams: {
+        project_id: this.project_id,
+        project_name: this.project_name,
+      },
+    });
   }
 
   getallusers() {
@@ -131,118 +139,85 @@ export class ProjectTaskDetailsComponent implements OnInit {
     this.inplace5.deactivate();
   }
 
-  inplaceActivateTaskName(field){
-    this[field] = this.task_details[field] 
+  inplaceActivate(field) {
+    if (field == "endDate") {
+      this.endDate = moment(this.task_details.endDate).format("YYYY-MM-DD");
+      return;
+    }
+    this[field] = this.task_details[field];
+    console.log(this.resources);
     // e.deactivate();
-
   }
 
-  onUpdateTaskName(field){
-    this.task_details[field] = this[field];
-    this.updatetask();
-    this.onDeactivate();
+  onUpdateDetails(field) {
+    if (field == "percentageComplete") {
+      if (this.percentageComplete > 100) {
+        this.percentageComplete = 100;
+      } else {
+        this.task_details[field] = String(this[field]);
+        this.updatetask();
+        this.onDeactivate();
+        console.log();
+      }
+    } else {
+      this.task_details[field] = this[field];
+      this.updatetask();
+      this.onDeactivate();
+    }
   }
 
-  inplaceActivateResource(){
-    this.edit_resource_field = this.task_details.resources 
-
+  inplaceActivateDesc() {
+    this.task_desc = this.task_details.description;
   }
 
-  onUpdateResourceDetails(){
-    this.task_details.resources = this.edit_resource_field
-    this.updatetask();
-    this.inplace1.deactivate();
-  }
-
-  inplaceActivateStatus(){
-    this.edit_status_field = this.task_details.status 
-
-  }
-
-  onUpdateStatusDetails(){
-    this.task_details.status = this.edit_status_field;
-    this.updatetask();
-    this.inplace2.deactivate();
-  }
-
-  inplaceActivateDate(){
-    // this.due_date = this.task_details.endDate 
-    this.due_date = moment(this.task_details.endDate).format("YYYY-MM-DD");
-
-  }
-
-  onUpdateDueDate(){
-    this.task_details.endDate = this.due_date
-    this.updatetask();
-    this.inplace4.deactivate();
-  }
-
-  inplaceActivateDesc(){
-    this.task_desc = this.task_details.description 
-  }
-
-  onUpdateDesc(){
+  onUpdateDesc() {
     this.task_details.description = this.task_desc.toString();
     this.updatetask();
     this.inplace3.deactivate();
   }
 
-  inplaceActivatePercent(){
-    this.edit_percentage_field = this.task_details.percentageComplete;
-  }
-
-  onUpdatePercent(){
-    if(this.edit_percentage_field >100){
-      this.edit_percentage_field = 100
-    }else{
-      this.task_details.percentageComplete = this.edit_percentage_field
-      this.updatetask();
-      this.inplace5.deactivate();
-    }
-  }
-
   closeOverlay(event) {
-    console.log(event)
+    console.log(event);
     this.hiddenPopUp = event;
     this.isprojectCreateForm = false;
   }
 
-  taskAttachments(){
+  taskAttachments() {
     this.hiddenPopUp = true;
     this.isprojectCreateForm = false;
     this.columns_list = [
-      {ColumnName: "fullName",DisplayName:"Document Name"},
-      { ColumnName: "user_role",DisplayName:"Attached Date"},
+      { ColumnName: "fullName", DisplayName: "Document Name" },
+      { ColumnName: "user_role", DisplayName: "Attached Date" },
       // { ColumnName: "actions",DisplayName:"Actions"},
     ];
   }
 
-  onClikCreateProject(){
-    
-  }
+  onClikCreateProject() {}
 
-  deleteuserById(rowData){
-
-  }
+  deleteuserById(rowData) {}
 
   postcomments() {
     if (this.add_comment != "") {
-      this.added_comments_list =[]
+      this.added_comments_list = [];
       // this.added_comments_list = this.task_details.history;
       let idnumber = this.taskcomments_list.length + 1;
       this.added_comments_list.push({
         id: idnumber,
         comments: this.add_comment,
         // createdTimestamp: new Date().getTime(),
-        createdBy : localStorage.getItem('firstName')+' '+ localStorage.getItem('lastName'),
-        createdUserEmail: localStorage.getItem('ProfileuserId')
+        createdBy:
+          localStorage.getItem("firstName") +
+          " " +
+          localStorage.getItem("lastName"),
+        createdUserEmail: localStorage.getItem("ProfileuserId"),
       });
       this.updatetask();
     }
   }
 
- async getTaskAttachments() {
-   await this.rest_api.getTaskAttachments(this.params_data.project_id, this.params_data.task_id)
+  async getTaskAttachments() {
+    await this.rest_api
+      .getTaskAttachments(this.params_data.project_id, this.params_data.task_id)
       .subscribe((data) => {
         // this.taskattacments = data;
         // if (this.taskattacments.length == 0) {
@@ -252,79 +227,76 @@ export class ProjectTaskDetailsComponent implements OnInit {
         // }
       });
   }
-  descChanges(str){
+  descChanges(str) {
+    //   var parser = new DOMParser();
+    // var doc = parser.parseFromString(str, 'text/html');
+    // console.log(doc)
+    // console.log(doc.body)
+    // return doc.body;
 
-  //   var parser = new DOMParser();
-	// var doc = parser.parseFromString(str, 'text/html');
-  // console.log(doc)
-  // console.log(doc.body)
-	// return doc.body;
+    let spanEl: HTMLElement = document.createElement("div");
+    spanEl.innerText = str;
+    return spanEl.outerHTML;
 
-  let spanEl: HTMLElement = document.createElement('div');
-      spanEl.innerText = str;
-      return spanEl.outerHTML
+    var dom = document.createElement("div");
+    dom.innerHTML = str;
+    return dom;
 
-  var dom = document.createElement('div');
-	dom.innerHTML = str;
-	return dom
+    //   var dom = document.createElement('div');
+    // dom.innerHTML = str;
+    // return dom;
+    //     var wrapper= document.createElement('div');
+    // wrapper.innerHTML= str;
+    // var div= wrapper.firstChild;
+    // return div
 
+    // var doc = new DOMParser().parseFromString(str, "text/xml")
+    // return doc
 
-  //   var dom = document.createElement('div');
-	// dom.innerHTML = str;
-	// return dom;
-//     var wrapper= document.createElement('div');
-// wrapper.innerHTML= str;
-// var div= wrapper.firstChild;
-// return div
-
-// var doc = new DOMParser().parseFromString(str, "text/xml")
-// return doc
-
-  // var dom = document.createElement('div');
-	// dom.innerHTML = str;
-	// return dom;
+    // var dom = document.createElement('div');
+    // dom.innerHTML = str;
+    // return dom;
   }
   updatetask() {
     // if (this.updatetaskForm.valid) {
-      // this.task_details
+    // this.task_details
 
-      // let taskupdatFormValue = this.updatetaskForm.value;
-      let taskupdatFormValue = this.task_details;
-      // taskupdatFormValue["id"] = this.selectedtask.id;
-      taskupdatFormValue["percentageComplete"] = "10";
-      taskupdatFormValue["comments"] = this.added_comments_list;
-      taskupdatFormValue["history"] = this.taskhistory_list;
-      // taskupdatFormValue["endDate"] = this.endDate;
-      // taskupdatFormValue["taskName"] = this.taskname;
-      // if (
-      //   this.optionValue == "As-Is Process" ||
-      //   this.optionValue == "To-Be Process"
-      // ) {
-      //   taskupdatFormValue["process"] = this.bpm_process_list.find(
-      //     (each) =>
-      //       each.correlationID == this.updatetaskForm.value.correlationID
-      //   ).processId;
-      // }
-      // taskupdatFormValue["taskCategory"]=this.taskcategory
-      this.rest_api.updateTask(taskupdatFormValue).subscribe(
-        (res) => {
-          this.taskcomments_list = this.added_comments_list
-          this.add_comment='';
-          this.gettask();
-          // let status: any = res;
-          // if (status.errorMessage == undefined) {
-          //   Swal.fire("Success", "Task Updated Successfully !!", "success");
-          // } else {
-          //   Swal.fire("Error", status.errorMessage, "error");
-          // }
-        },
-        (err) => {
-          Swal.fire("Error", "Something Went Wrong", "error");
-        }
-      );
+    // let taskupdatFormValue = this.updatetaskForm.value;
+    let taskupdatFormValue = this.task_details;
+    // taskupdatFormValue["id"] = this.selectedtask.id;
+    // taskupdatFormValue["percentageComplete"] = "10";
+    taskupdatFormValue["comments"] = this.added_comments_list;
+    taskupdatFormValue["history"] = this.taskhistory_list;
+    // taskupdatFormValue["endDate"] = this.endDate;
+    // taskupdatFormValue["taskName"] = this.taskname;
+    // if (
+    //   this.optionValue == "As-Is Process" ||
+    //   this.optionValue == "To-Be Process"
+    // ) {
+    //   taskupdatFormValue["process"] = this.bpm_process_list.find(
+    //     (each) =>
+    //       each.correlationID == this.updatetaskForm.value.correlationID
+    //   ).processId;
+    // }
+    // taskupdatFormValue["taskCategory"]=this.taskcategory
+    this.rest_api.updateTask(taskupdatFormValue).subscribe(
+      (res) => {
+        this.taskcomments_list = this.added_comments_list;
+        this.add_comment = "";
+        this.gettask();
+        // let status: any = res;
+        // if (status.errorMessage == undefined) {
+        //   Swal.fire("Success", "Task Updated Successfully !!", "success");
+        // } else {
+        //   Swal.fire("Error", status.errorMessage, "error");
+        // }
+      },
+      (err) => {
+        Swal.fire("Error", "Something Went Wrong", "error");
+      }
+    );
     // } else {
     //   alert("please fill all details");
     // }
   }
-
 }
