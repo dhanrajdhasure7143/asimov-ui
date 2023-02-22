d46bf69c60747ac20702c15f836fc164f231b576
Malleswari Devarapalli <durga.devarapalli@epsoftinc.com>
2023-02-22 18:25:20 +0530

    taskdetails,attachdocfoldertree,somecssfixes

--- src/app/pages/projects/projects-list-screen/projects-list-screen.component.ts
+++ src/app/pages/projects/projects-list-screen/projects-list-screen.component.ts
@@ -301,7 +301,7 @@ export class ProjectsListScreenComponent implements OnInit {
       // },
       {
         ColumnName: "action",
-        DisplayName: "",
+        DisplayName: "Actions",
         ShowGrid: true,
         ShowFilter: false,
         sort: false,
