9784492f3f4ab65e8436e8fdf016663586801764
pranathi.boppana <pranathi.boppana@epsoftinc.com>
2023-03-02 13:01:54 +0530

    [Ezflw-2026] Json validation changes connection manager

--- src/app/pages/rpautomation/forms/rpa-connection-manager-form/rpa-connection-manager-form.component.ts
+++ src/app/pages/rpautomation/forms/rpa-connection-manager-form/rpa-connection-manager-form.component.ts
@@ -23,11 +23,16 @@ export class RpaConnectionManagerFormComponent implements OnInit {
   isRequest: boolean = false;
   isResponse: boolean = false;
   attribute = [];
-
+  public result ={};
+  public reactiveForm:FormGroup;
+  validateJSON:boolean=false;
   constructor(
     private formBuilder: FormBuilder,
     private rest_api: RestApiService
   ) {
+    this.reactiveForm=this.formBuilder.group({
+      result:[""]
+    })
     this.createItem();
   }
 
@@ -132,6 +137,19 @@ export class RpaConnectionManagerFormComponent implements OnInit {
   }
 
   addHeader() {}
+  isJsonValid(){
+    let jsonData=this.connectorForm.get('bodyRaw').value;
+      try{
+        JSON.parse(jsonData);
+        this.validateJSON= false;
+      }
+      catch(e)
+      {
+        console.log(e)
+        this.validateJSON=true;
+      }
+  }
+
 
   actionChange(event) {
     if (event == "Authenticated") {
