64d988bec5eeb8279b76c44f2851414ffdbedd1f
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-03-14 10:09:28 +0530

    letters added in project list

--- src/app/shared/pipes/user-letters.pipe.ts
+++ src/app/shared/pipes/user-letters.pipe.ts
@@ -9,7 +9,7 @@ export class UserLettersPipe implements PipeTransform {
     if(users_list.length > 0 && userId){
       let user=users_list.find(item=>item.userId.userId==userId);
       if(user.userId.image==null){
-        return user!=undefined?(user.userId.firstName.charAt(0)+ " "+user.userId.lastName.charAt(0)):userId;
+        return user!=undefined?(user.userId.firstName.charAt(0)+user.userId.lastName.charAt(0)):userId;
       }
   }
 }
