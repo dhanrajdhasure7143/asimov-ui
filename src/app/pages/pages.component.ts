9efeb7aca9e58442a2c185d5736de142fa371550
Malleswari Devarapalli <durga.devarapalli@epsoftinc.com>
2023-04-26 17:12:59 +0530

    spacingscss

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -34,7 +34,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     overflow: hidden;
   }
   .main{
-    height: calc(100% - 60px);
+    height: calc(100% - 55px);
     background-color: #d9dfe4 !important;
     z-index: 1;
     position: relative;
