23af327aafe806a54e3595f26e31a0a296373ee3
sai.gangala <sai.gangala@epsoftinc.com>
2023-05-02 12:43:09 +0530

    [EZFLW - 2776] Header Fixes

--- src/app/pages/rpautomation/forms/rpa-connection-manager-form/rpa-connection-manager-form.component.ts
+++ src/app/pages/rpautomation/forms/rpa-connection-manager-form/rpa-connection-manager-form.component.ts
@@ -180,9 +180,10 @@ export class RpaConnectionManagerFormComponent implements OnInit {
         "description" : "",
       }
       
-        this.requestJson_body=[]
+      console.log(this.headerForm)
         let obj={}
-        this.selectedOne.forEach(ele=>{
+        this.headerForm.forEach(ele=>{
+          console.log(ele.check,"ele")
           obj[ele["encodedKey"]]=ele["encodedValue"];
         })
        
@@ -194,7 +195,7 @@ export class RpaConnectionManagerFormComponent implements OnInit {
         "contentType":"application/json",
         "httpHeaders": obj,
         "type":"API",
-        "requestPayload": this.connectorForm.get("request").value == null ? "" : this.connectorForm.get("request").value.replace(/\s/g, "")
+        "requestPayload": this.connectorForm.get("request").value == null ? "" : this.connectorForm.get("request").value
     }
     req_body["configuration"]=JSON.stringify(object);
     }
@@ -798,7 +799,7 @@ export class RpaConnectionManagerFormComponent implements OnInit {
         contentType: "application/json",
         httpHeaders: obj,
         "type":"API",
-        "requestPayload":this.connectorForm.get("request").value.replace(/\s/g, "")
+        "requestPayload":this.connectorForm.get("request").value
       };
       req_body["configuration"] = JSON.stringify(object);
     }
