642b5458b50b6900f28b23e26e3e92af6e059e97
krishna.jandhyala <krishnavardhana.jandhyala@epsoftinc.com>
2023-07-12 11:20:56 +0530

    History changes

--- src/app/pages/copilot/copilot-chat/copilot-chat.component.ts
+++ src/app/pages/copilot/copilot-chat/copilot-chat.component.ts
@@ -11,10 +11,17 @@ import { Router } from '@angular/router';
 export class CopilotChatComponent implements OnInit {
   display: boolean = false;
   historyOpen:boolean = false;
-
+  historyList:any=[]
+  message:any
   constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.historyList=[
+      {label:"Process Graph"},
+      {label:"RPA"},
+      {label:"BPS"},
+      {label:"PI"}
+    ]
   
 }  
 
@@ -22,14 +29,13 @@ showDialog() {
   this.display = true;
 }
 
-openHistory(){
-  this.historyOpen = true
-}
-
-openChat2()
-{
+openChat2(){
   this.router.navigate(["./pages/copilot/copilot-chat"]) 
 }
 
-
+sendMessage(){
+  console.log(this.message);
+  this.historyList.push({label:this.message})
+  this.message=""
+}
 }
