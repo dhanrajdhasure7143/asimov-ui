9c75a54f8e04b65352f0fd161db60d0fc0ee60c7
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-07-19 15:21:37 +0530

    rpa configuration changes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -40,5 +40,6 @@ export const environment = {
     ipcTenant: "28b22b35-9dc5-4860-b5b5-559855d9618c",
     isChatEnable : false,
     isProcessLogsEnable: false,
+    isCopilotEnable : false
   };
   
