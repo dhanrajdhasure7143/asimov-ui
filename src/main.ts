02361705772d6f0f7a5f391d5338eb524886c6e6
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-10-19 12:31:57 +0530

    service worker changes

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
   if ('serviceWorker' in navigator && environment.production) {
-    navigator.serviceWorker.register('/ngsw-worker.js');
+    navigator.serviceWorker.register('/ngsw-worker.js?v=2') // Increment the version here
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
   }
 }).catch(err => console.log(err));
