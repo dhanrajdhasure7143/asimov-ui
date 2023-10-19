5030270055f0b5307be71a7feb5c6f632adf2ece
Ranjith Sigiri <ranjith.sigiri@epsoftinc.com>
2023-10-19 04:11:30 -0500

    Revert "Merge branch 'feature/disableServiceWorker' into 'Sprint-95'"
    This reverts merge request !2631

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
   if ('serviceWorker' in navigator && environment.production) {
-    navigator.serviceWorker.register('/ngsw-worker.js?v=2') // Increment the version here
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
+    navigator.serviceWorker.register('/ngsw-worker.js');
   }
 }).catch(err => console.log(err));
