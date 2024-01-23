2fc3440a8d9c43262e675e1d6d39485c2497fa73
Praveen Bokkala <praveenbokkala@epsoftinc.com>
2024-01-23 19:30:35 +0530

    [E30S-230] CRUD API'sintegration for EZAsk admon screen

--- src/app/pages/administration/manage-customer-bot/manage-customer-bot.component.ts
+++ src/app/pages/administration/manage-customer-bot/manage-customer-bot.component.ts
@@ -6,12 +6,13 @@ import { LoaderService } from 'src/app/services/loader/loader.service';
 import { columnList } from 'src/app/shared/model/table_columns';
 import { toastMessages } from 'src/app/shared/model/toast_messages';
 import { ToasterService } from 'src/app/shared/service/toaster.service';
+import { CopilotService } from '../../services/copilot.service';
 
 enum BotContentType {
-  Web = 'Web',
-  Document = 'Document',
+  Web = 'WEB',
+  Document = 'DOC',
   WebAndDocument = 'Web and Document',
-  Public = 'Public',
+  Public = 'PUB',
 }
 
 @Component({
@@ -29,10 +30,10 @@ export class ManageCustomerBotComponent implements OnInit {
   nextRecordId = 1;
 
   manageBotList = [
-    { id: '1', manageBotName: 'Customer Support Bot', botGreetingMessage: 'Sample Message', primaryPrompt: 'Sample prompt', responsePrefix: 'Sample prefix', isHallucination: false, isActive: true, isDelete: true, botKey: 'SampleKey', embedUrl: 'http://www.google.com', botContentType: 'Document', include_sites: '', exclude_sites: '' },
-    { id: '2', manageBotName: 'Agent Support Bot', botGreetingMessage: 'Sample', primaryPrompt: 'Sample prompt', responsePrefix: 'Sample prefix', isHallucination: true, isActive: true, isDelete: false, botKey: 'SampleKey', embedUrl: 'http://www.google.com', botContentType: 'Web', include_sites: 'SampleSite', exclude_sites: 'SampleSite' },
-    { id: '3', manageBotName: 'Employee Support Bot', botGreetingMessage: 'Sample Greeting', primaryPrompt: 'Sample prompt', responsePrefix: 'Sample prefix', isHallucination: false, isActive: true, isDelete: true, botKey: 'SampleKey', embedUrl: 'http://www.google.com', botContentType: 'Document and Web', include_sites: '', exclude_sites: '' },
-    { id: '4', manageBotName: 'Sample Record', botGreetingMessage: 'Sample Data', primaryPrompt: 'Sample prompt', responsePrefix: 'Sample prefix', isHallucination: true, isActive: true, isDelete: false, botKey: 'SampleKey', embedUrl: 'http://www.google.com', botContentType: 'Sample Data', include_sites: '', exclude_sites: '' },
+    // { id: '1', manageBotName: 'Customer Support Bot', greetingMessage: 'Sample Message', primaryPrompt: 'Sample prompt', respPrefix: 'Sample prefix', hallucinationAllowed: false, isActive: true, isDelete: true, botKey: 'SampleKey', customerSupportBotEmbedUrl: 'http://www.google.com', customerSupportBotSource: 'Document', include_sites: '', exclude_sites: '' },
+    // { id: '2', manageBotName: 'Agent Support Bot', greetingMessage: 'Sample', primaryPrompt: 'Sample prompt', respPrefix: 'Sample prefix', hallucinationAllowed: true, isActive: true, isDelete: false, botKey: 'SampleKey', customerSupportBotEmbedUrl: 'http://www.google.com', customerSupportBotSource: 'Web', include_sites: 'SampleSite', exclude_sites: 'SampleSite' },
+    // { id: '3', manageBotName: 'Employee Support Bot', greetingMessage: 'Sample Greeting', primaryPrompt: 'Sample prompt', respPrefix: 'Sample prefix', hallucinationAllowed: false, isActive: true, isDelete: true, botKey: 'SampleKey', customerSupportBotEmbedUrl: 'http://www.google.com', customerSupportBotSource: 'Document and Web', include_sites: '', exclude_sites: '' },
+    // { id: '4', manageBotName: 'Sample Record', greetingMessage: 'Sample Data', primaryPrompt: 'Sample prompt', respPrefix: 'Sample prefix', hallucinationAllowed: true, isActive: true, isDelete: false, botKey: 'SampleKey', customerSupportBotEmbedUrl: 'http://www.google.com', customerSupportBotSource: 'Sample Data', include_sites: '', exclude_sites: '' },
   ];
 
   botContentOptions: SelectItem[] = Object.values(BotContentType).map((value) => ({ label: value, value }));
@@ -46,13 +47,14 @@ export class ManageCustomerBotComponent implements OnInit {
     private toastMessages: toastMessages,
     private confirmationService: ConfirmationService,
     private router: Router,
+    private rest_api: CopilotService,
   ) { }
 
   ngOnInit(): void {
     this.loader.show();
     this.columns_list = this.columns.manage_cutomer_support_bot_coloumns;
-    this.table_searchFields = ['manageBotName', 'botContentType'];
     this.initializeBotForm();
+    this.getAllCustomerBots();
     setTimeout(() => {
       this.loader.hide();
     }, 1000);
@@ -60,18 +62,22 @@ export class ManageCustomerBotComponent implements OnInit {
 
   private initializeBotForm() {
     const formControls = {
-      manageBotName: ['', [Validators.required, Validators.maxLength(50)]],
-      botGreetingMessage: ['', [Validators.required, Validators.maxLength(50)]],
+      customerSupportBotName: ['', [Validators.required, Validators.maxLength(50)]],
+      greetingMessage: ['', [Validators.required, Validators.maxLength(50)]],
       primaryPrompt: [''],
-      responsePrefix: [''],
-      embedUrl: ['www.google.com', [Validators.required, Validators.maxLength(50)]],
-      botKey: ['SampleKey', [Validators.required, Validators.maxLength(50)]],
-      isHallucination: [false],
+      respPrefix: [''],
+      customerSupportBotEmbedUrl: [''],
+      botKey: [''],
+      // customerSupportBotEmbedUrl: ['', [Validators.required, Validators.maxLength(50)]],
+      // botKey: ['', [Validators.required, Validators.maxLength(50)]],
+      hallucinationAllowed: [false],
       isActive: [false],
       isDelete: [false],
-      botContentType: ['', [Validators.required, Validators.maxLength(50)]],
-      include_sites: [{ value: '', disabled: true }, [Validators.required]],
-      exclude_sites: [{ value: '', disabled: true }, [Validators.required]],
+      customerSupportBotSource: ['', [Validators.required, Validators.maxLength(50)]],
+      // include_sites: [{ value: '', disabled: true }, [Validators.required]],
+      // exclude_sites: [{ value: '', disabled: true }, [Validators.required]],
+      include_sites: ['', [Validators.required]],
+      exclude_sites: ['', [Validators.required]],
     };
     this.manageBotForm = this.formBuilder.group(formControls);
   }
@@ -82,6 +88,20 @@ export class ManageCustomerBotComponent implements OnInit {
     this.initializeBotForm();
   }
 
+  getAllCustomerBots() {
+    this.loader.show();
+    this.rest_api.getCustomerBots().subscribe((botlist) => {
+      this.manageBotList = botlist;
+      this.loader.hide();
+      this.table_searchFields = ['customerSupportBotName', 'customerSupportBotSource'];
+      console.log("custBots", botlist);
+      },
+      (err)=>{
+        console.error("Error fetching customer bots:", err);
+      }
+    );
+  }
+  
   viewDetails(event: any) {
     console.log(event);
     this.router.navigate(["/pages/admin/view-customer-bot-details"], {
@@ -90,8 +110,9 @@ export class ManageCustomerBotComponent implements OnInit {
   }
  
 
-  deleteBot(event) {
-    const recordIdToDelete = event.id;
+  deleteCustomerSupportBot(event) {
+    const recordIdToDelete = event.customerSupportBotId;
+    const recordName = event.customerSupportBotName;
     this.confirmationService.confirm({
       message: 'Do you want to delete this record? This can\'t be undone.',
       header: 'Are you sure?',
@@ -104,10 +125,17 @@ export class ManageCustomerBotComponent implements OnInit {
       acceptIcon: 'null',
       key: 'positionDialog',
       accept: () => {
-        this.loader.show();
-        this.manageBotList = this.manageBotList.filter((record) => record.id !== recordIdToDelete);
-        this.loader.hide();
-        this.toastService.showSuccess('Deleted Successfully', 'response');
+        let response
+        // this.loader.show();
+        this.rest_api.deleteCustomerBot(recordIdToDelete).subscribe((res:any)=>{
+          response = res
+          this.toastService.showSuccess(recordName,'delete');
+          this.getAllCustomerBots();
+        },(err) => {
+          this.toastService.showError(response.errorMessage);
+          this.loader.hide();
+          this.getAllCustomerBots();
+        })
       },
       reject: () => { },
     });
@@ -125,23 +153,36 @@ export class ManageCustomerBotComponent implements OnInit {
     }
   }
 
-  formSubmit() {
+  saveCustomerSupportBot() {
     this.loader.show();
-    const recordId = Date.now();
-    const sampleObj = {
-      id: recordId,
-      ...this.manageBotForm.value,
-    };
-    console.log("SamplePayload",sampleObj);
-    
-    this.manageBotList.push(sampleObj);
-    this.toastService.showSuccess('Saved Successfully', 'response');
-    setTimeout(() => {
+    let req_body={
+        "createdDate": "",
+        "customerSupportBotId": "",
+        "userId": "",
+        "tenantId": "",
+        "customerSupportBotName": this.manageBotForm.value.customerSupportBotName,
+        "greetingMessage": this.manageBotForm.value.greetingMessage,
+        // "primaryPrompt": "You are an AI customer service assistant. Follow these instructions while responding. \\n\\n 1. Response should not mention about \\\"source_content\\\". \\n\\n 2. Response should be informative, visual, logical and actionable. \\n\\n 3. Responses should be positive and engaging. \\n\\n 4. Responses should avoid being vague, off-topic. \\n\\n 5. If you don't find answer in \\\"source_content\\\" then respond only with 'NO_ANSWER', Do not answer with anything else. \\n\\n 6. Only when customer ask for human agent, add text 'AGENT' to the response. \\n\\n 7. Give the response in well formatted HTML format. \\n\\n source_content:",
+        "primaryPrompt": this.manageBotForm.value.primaryPrompt,
+        "respPrefix": this.manageBotForm.value.respPrefix,
+        "customerSupportBotSource": this.manageBotForm.value.customerSupportBotSource,
+        "customerSupportBotEmbedUrl": "",
+        "botKey": "",
+        "customerSupportBotCollection": "",
+        "includeSites": this.manageBotForm.value.include_sites,
+        "excludeSites": this.manageBotForm.value.exclude_sites,
+        "botDisplayName": "Ask DL",
+        "hallucinationAllowed": this.manageBotForm.value.hallucinationAllowed,
+        "active": this.manageBotForm.value.isActive,
+        "deleted": this.manageBotForm.value.isDelete
+    }
+    this.rest_api.saveCustomerBot(req_body).subscribe((res:any)=>{
       this.loader.hide();
-    }, 1000);
-    this.nextRecordId++;
-    this.hiddenPopUp = false;
-    this.manageBotForm.reset();
+      let bot_Name = req_body.customerSupportBotName
+      this.toastService.showSuccess(bot_Name,'save');
+      console.log("req_body", req_body);
+      console.log("Responce", res);
+    })
   }
 
 
@@ -196,8 +237,8 @@ export class ManageCustomerBotComponent implements OnInit {
 
   resetbotform() {
     this.manageBotForm.reset();
-    this.manageBotForm.get('embedUrl').setValue('http://www.google.com');
-    this.manageBotForm.get('botKey').setValue('SampleKey');
+    // this.manageBotForm.get('customerSupportBotEmbedUrl').setValue('http://www.google.com');
+    // this.manageBotForm.get('botKey').setValue('SampleKey');
   }
 
   closeOverlay(event: any) {
