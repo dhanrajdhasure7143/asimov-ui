31d3d8e717f291c24ecf05de0a90c35b2df199ce
Praveen Bokkala <praveenbokkala@epsoftinc.com>
2024-02-21 13:59:18 +0530

    Added SoApprovalComponent to declarations and imports.

---
+++ src/app/so-approval/so-approval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-so-approval',
+  templateUrl: './so-approval.component.html',
+  styleUrls: ['./so-approval.component.css']
+})
+export class SoApprovalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
