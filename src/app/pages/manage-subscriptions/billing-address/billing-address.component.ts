d5c26e6f869105e65b503d62a77f9595c3538fbb
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2024-03-29 18:12:28 +0530

    billing ionfo changes

--- src/app/pages/manage-subscriptions/billing-address/billing-address.component.ts
+++ src/app/pages/manage-subscriptions/billing-address/billing-address.component.ts
@@ -46,11 +46,11 @@ export class BillingAddressComponent implements OnInit {
       name: ["", Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]+(\\s[a-zA-Z]+)*$'), Validators.maxLength(50)])],
       country: ["", Validators.compose([Validators.required])],
       city: ["", Validators.compose([Validators.required, Validators.maxLength(50)])],
-      state: ["", Validators.compose([Validators.required, Validators.maxLength(50)])],
+      // state: ["", Validators.compose([Validators.required, Validators.maxLength(50)])],
       postalcode: ["", Validators.compose([Validators.required, Validators.maxLength(6), Validators.pattern('^[0-9]+$')])],
       addressLine1: ["", Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9-/,]+(\\s[a-zA-Z0-9-/]+)*$'), Validators.maxLength(50)])],
       addressLine2: ["", Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9-/,]+(\\s[a-zA-Z0-9-/]+)*$'), Validators.maxLength(50)])],
-      phoneNumber: ["", Validators.compose([Validators.required, Validators.maxLength(10), Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")])],
+      // phoneNumber: ["", Validators.compose([Validators.required, Validators.maxLength(10), Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")])],
       email: ["", Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"), Validators.maxLength(50)])],
     });
     this.getCountries();
@@ -59,7 +59,6 @@ export class BillingAddressComponent implements OnInit {
   
   getCountries() {
     this.countryInfo = Country.getAllCountries();
-    console.log()
   }
 
   // onChangeCountry(countryValue) {
@@ -101,9 +100,7 @@ export class BillingAddressComponent implements OnInit {
     this.stateInfo = State.getAllStates();
     this.cityInfo = [];
     const matchingCountryDetails = this.countryInfo.find((item: any) => item.name == countryValue || item.isoCode == countryValue);
-    console.log("matchingCountryDetails", matchingCountryDetails);
     const matchingCountry = matchingCountryDetails.isoCode;
-    console.log("matchingCountry", matchingCountry);
     if (!matchingCountry) {
       this.countryName = countryValue;
       this.countryInfo.push({ name: countryValue, countryCode: this.countryName, isoCode: '' });
@@ -112,29 +109,23 @@ export class BillingAddressComponent implements OnInit {
     else {
       this.countryName = matchingCountryDetails.name;
       this.stateInfo = this.stateInfo.filter((state: any) => state.countryCode === matchingCountry);
-      console.log("stateInfo:", this.stateInfo);
       this.errorMessage = '';
     }
   }
 
   onChangeState(stateValue) {
-    console.log("stateValue", stateValue);
     this.cityInfo = City.getAllCities();
     const matchingStateDetails = this.stateInfo.find((state: any) => state.name === stateValue);
     const matchingState = matchingStateDetails.name;
-    console.log("matchingState", matchingState);
 
     if (!matchingState) {
       this.stateName = stateValue;
       this.stateInfo.push({ name: stateValue, countryCode: this.countryName, isoCode: '' }); // Add the state to the stateInfo array
-      console.log("statename:", this.stateName);
     } 
     
     else {
       this.stateName = matchingState;
-      console.log("cityInfo:", this.cityInfo);
       this.cityInfo = this.cityInfo.filter((city: any) => city.countryCode === matchingState.countryCode && city.stateCode === matchingState.isoCode);
-      console.log("cityInfo:", this.cityInfo);
       this.errorMessage1='';
     }
   }
@@ -159,7 +150,7 @@ export class BillingAddressComponent implements OnInit {
         email: this.billingForm.get('email').value,
         address: {
           country: this.billingForm.get('country').value,
-          "state": this.billingForm.get('state').value,
+          // "state": this.billingForm.get('state').value,
           "city": this.billingForm.get('city').value,
           line1: this.billingForm.get('addressLine1').value,
           line2: this.billingForm.get('addressLine2').value,
@@ -167,13 +158,9 @@ export class BillingAddressComponent implements OnInit {
         }
       };
       
-      console.log("billing form:", this.billingForm.value);
-      this.api.updateBillingInfo(payload).subscribe((data) => {
-        console.log("updated payload data:", payload);
-        console.log("data:", data);
-        if (data) {
+      this.api.updateBillingInfo(payload).subscribe((data:any) => {
+        if (data.code == 4200) {
           this.toastService.showSuccess("Updated successfully!",'response');
-          // this.billingForm.reset();
           this.getBillingInfo();
           this.spinner.hide();
         }
@@ -188,25 +175,27 @@ export class BillingAddressComponent implements OnInit {
   getBillingInfo() {
     this.spinner.show();
     this.api.getBillingInfo().subscribe((data:any) => {
-      console.log("gettingdata:",data);
       this.spinner.hide();
       if(data){
         this.billingData = data.message;       
         this.billingAddress = data.message.address;
-        setTimeout(()=>{
-          this.billingForm.get("country").setValue(this.countryName);
-          this.billingForm.get("state").setValue(this.stateName);
-          this.billingForm.get("city").setValue(this.cityName);
-        },300);
+        // setTimeout(()=>{
+        //   this.billingForm.get("country").setValue(this.countryName);
+        //   this.billingForm.get("state").setValue(this.stateName);
+        //   this.billingForm.get("city").setValue(this.cityName);
+        // },300);
         this.billingForm.get("name").setValue(this.billingData["name"]);
         this.billingForm.get("email").setValue(this.billingData["email"]);
         this.billingForm.get("postalcode").setValue(this.billingAddress["postalCode"]);
         this.billingForm.get("addressLine1").setValue(this.billingAddress["line1"]);
         this.billingForm.get("addressLine2").setValue(this.billingAddress["line2"]);
+        this.billingForm.get("city").setValue(this.billingAddress["city"]);
+        this.billingForm.get("country").setValue(this.billingAddress["country"]);
+
         // this.billingForm.get("phoneNumber").setValue(this.billingAddress.["phone"]);
-        this.onChangeCountry(this.billingAddress.country);
-        this.onChangeState(this.billingAddress.state);
-        this.onChangeCity(this.billingAddress.city);
+        // this.onChangeCountry(this.billingAddress.country);
+        // this.onChangeState(this.billingAddress.state);
+        // this.onChangeCity(this.billingAddress.city);
         this.editButton=true;
       }
       },
