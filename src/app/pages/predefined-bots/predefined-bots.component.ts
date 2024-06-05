0157188c80853e10d20f09250c9a1fd0580eb138
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2024-06-05 14:18:31 +0530

    chat bot removed from main module and added in predefined Module

--- src/app/pages/predefined-bots/predefined-bots.component.ts
+++ src/app/pages/predefined-bots/predefined-bots.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-predefined-bots',
@@ -11,5 +12,26 @@ export class PredefinedBotsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+
+    setTimeout(() => {
+      const iframe = document.getElementById('iframeRef') as HTMLIFrameElement;
+      if(iframe){
+        console.log("testing.........iframe enabled")
+      iframe.contentWindow.postMessage({ action: 'botKey', bot_key: environment.ezChatBotKey,accesstoken:localStorage.getItem("accessToken") }, '*');
+      }
+    }, 2000);
+    
+    window.addEventListener('message', event => {
+      const message = event.data;
+      const iframe = document.getElementById('iframeRef') as HTMLIFrameElement;
+      if(iframe){
+          iframe.style.height = message.height;
+          iframe.style.width = message.width;
+      }
+  });
+
+}
 
 }
