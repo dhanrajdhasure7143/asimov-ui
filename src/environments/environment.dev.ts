ddf9bc240d88662f01a94c21b124823b160fb878
aravind.chedurupally <aravind.chedurupally@epsoftinc.com>
2024-08-16 15:46:14 +0530

    changed ezflow chat bot key

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -53,6 +53,6 @@ export const environment = {
     stripeKey:"pk_test_51PjIMqJZCEXZ8Zrct2ntJVGOgqwWLvEBFV7DUJwm7nLEwoNpKClC2GuDs3CPUUedXpv5m0pDTHLJwJKrFQC9fg1I00zKCS3psO",
     python_llm:"http://llmpython.dev.epsoftinc.in/",
     ezaskUrl: "http://ezask.dev.epsoftinc.in",
-    ezChatBotKey:"0p4kvugbxY7VE0Rw9/486/oGLzA5UBGFZSRHDLEpicxompCprTUcip7avMfguwE5LOprAOANqdop2F1+u9Lfx4zOdol0pnoMgCY=",
+    ezChatBotKey:"1STWkvY7eRPyOmOUq5kHRfGQ4u2rX4+vq6cAXyw3smk+jCjVtj2p9iF9xEfqRLbgDjNn1uM+q4B615AGqG1tIeDpntAvP1MKN38=",
     product:"AiAgents"
   };
