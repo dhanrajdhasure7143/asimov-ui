4a4f1345f3eb9694eae602cdc8e315a777118310
Praveen Bokkala <praveenbokkala@epsoftinc.com>
2024-09-09 11:38:55 +0530

    Navigation Issue fix and CSS for quantity buttons

--- src/app/pages/ai-agents/agent-pages/marketing-ai-sales-page.ts
+++ src/app/pages/ai-agents/agent-pages/marketing-ai-sales-page.ts
@@ -164,7 +164,7 @@ import { ToasterService } from 'src/app/shared/service/toaster.service';
       color: #fff;
       font-size: 18px;
       width: 30px;
-      height: 30px;
+      height: 34px;
       cursor: pointer;
       display: flex;
       align-items: center;
@@ -601,7 +601,7 @@ loadPredefinedBots() {
       return;
     }
     
-
+    let filteredUrls = this.router.url.split('&');
     let req_body = {
       // "price": filteredPriceIds,
       "priceData": filteredPriceIds.map(price => ({
@@ -612,7 +612,8 @@ loadPredefinedBots() {
       "successUrl": environment.paymentSuccessURL,
       // "cancelUrl": environment.paymentFailuerURL+"?token="+this.crypto.encrypt(this.userEmail)
     //   "cancelUrl": environment.paymentFailuerURL+"?token="+this.email+"&id="+this.selectedAgentId+"&quantity="+this.agentsQuantity+"&isYearly="+this.isYearly
-      "cancelUrl": environment.paymentFailuerURL+this.router.url+"&quantity="+this.agentsQuantity+"&isYearly="+this.isYearly
+      // "cancelUrl": environment.paymentFailuerURL+this.router.url+"&quantity="+this.agentsQuantity+"&isYearly="+this.isYearly
+      "cancelUrl": environment.paymentFailuerURL+filteredUrls[0]+'&'+filteredUrls[1]+"&quantity="+this.agentsQuantity+"&isYearly="+this.isYearly
     };
     // console.log("PLAN_ID's", req_body);
     
