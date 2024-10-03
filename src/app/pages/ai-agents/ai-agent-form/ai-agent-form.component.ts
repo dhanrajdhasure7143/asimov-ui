4fa65291533e715ae712f60631b332ff4aa69198
yogendra9951 <yogendramanikanta9951@gmail.com>
2024-10-03 23:17:46 +0530

    [code changes for delete files field level deleteing]

--- src/app/pages/ai-agents/ai-agent-form/ai-agent-form.component.ts
+++ src/app/pages/ai-agents/ai-agent-form/ai-agent-form.component.ts
@@ -2073,6 +2073,8 @@ handleHistoryTab (hist) {
                         this.toaster.toastSuccess("Files deleted successfully.");
                         if (typeof input === 'object') {
                           this.removeFilesFromForm(input);
+                        }else if(typeof input === 'string' && input === 'Files'){
+                          this.removeFilesFromForm1(selectedFiles);
                         }
                     },
                     (err) => {
@@ -2096,6 +2098,17 @@ removeFilesFromForm(deletedFile:any){
     }
   }) 
 }
+
+removeFilesFromForm1(deletedFiles: any[]) {
+  deletedFiles.forEach(deletedFile => {
+    if (this.attachmentMap[deletedFile.inputKey]) {
+      this.attachmentMap[deletedFile.inputKey] = this.attachmentMap[deletedFile.inputKey].filter(file => {
+        return file.fileNameWithUUID !== deletedFile.fileNameWithUUID;
+      });
+    }
+  });
+}
+
 
   downloadSubAgentHistoryAsExcel() {
     const historyData = this.historyToDownload;
